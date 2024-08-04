a371e84336fe62460b18e0678bdbae6316d36063
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2024-08-04 12:37:15 -0700

    Create 965. Univalued Binary Tree.java
    Link to the question:
    https://leetcode.com/problems/univalued-binary-tree/

---
+++ Binary Trees/Easy/965. Univalued Binary Tree.java
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isUnivalTree(TreeNode root) {
+
+        if(root == null){
+            return true;
+        }
+
+        if(root.left != null && root.val != root.left.val){
+            return false;
+        }
+
+        if(root.right != null && root.val != root.right.val){
+            return false;
+        }
+
+        return isUnivalTree(root.left) && isUnivalTree(root.right);
+
+
+        
+        // Queue<TreeNode> queue = new LinkedList<>();
+        // queue.add(root);
+
+        // while(!queue.isEmpty()){
+        //     TreeNode curr = queue.poll();
+
+        //     if(curr.left != null){
+        //         if(curr.val == curr.left.val){
+        //             queue.add(curr.left);
+        //         }
+        //         else{
+        //             return false;
+        //         }
+        //     }
+            
+        //     if(curr.right != null){
+        //         if(curr.val == curr.right.val){
+        //             queue.add(curr.right);
+        //         }
+        //         else{
+        //             return false;
+        //         }
+        //     }
+        // }
+
+        // return true;
+
+    }
+}
