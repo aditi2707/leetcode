0f3621dbf37ba0a9bcb7b4275a60e3954095af42
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2024-08-08 20:46:24 -0700

    Create 404. Sum of Left Leaves.java
    Link to the question:
    https://leetcode.com/problems/sum-of-left-leaves/

---
+++ Binary Trees/Easy/404. Sum of Left Leaves.java
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public int findLeft(TreeNode root, boolean isLeft){
+        if(root == null){
+            return 0;
+        }
+
+        if(root.left == null && root.right == null){
+            if(isLeft){
+                return root.val;
+            }
+            else{
+                return 0;
+            }
+        }
+
+        return findLeft(root.left, true) + findLeft(root.right, false);
+    }
+
+    
+    public int sumOfLeftLeaves(TreeNode root) {
+
+        if(root == null){
+            return 0;
+        }
+
+        return findLeft(root, false);
+        
+        // Queue<TreeNode> queue = new LinkedList<>();
+        // queue.add(root);
+        // int sum = 0;
+
+        // while(!queue.isEmpty()){
+        //     TreeNode curr = queue.poll();
+
+        //     if(curr.left != null){
+        //         queue.add(curr.left);
+        //         if(curr.left.left == null && curr.left.right == null){
+        //             sum += curr.left.val;
+        //         }
+        //     }
+        //     if(curr.right != null){
+        //         queue.add(curr.right);
+        //     }
+        // }
+
+        // return sum;
+    }
+}
