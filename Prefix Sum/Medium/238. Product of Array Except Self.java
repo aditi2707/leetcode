3ad83ca8216f5cbaca7f35363b48b1a2d9be5eca
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2024-09-07 14:03:12 -0700

    Create 238. Product of Array Except Self.java
    Link to the question:
    https://leetcode.com/problems/product-of-array-except-self/

---
+++ Prefix Sum/Medium/238. Product of Array Except Self.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        
+        // int[] prefix = new int[nums.length];
+        // int[] suffix = new int[nums.length];
+        // prefix[0] = 1;
+        // suffix[nums.length-1] = 1;
+
+        int[] ans = new int[nums.length];
+        ans[0] = 1;
+        ans[1] = 1;
+
+
+        for(int i = 1; i < nums.length; i++){
+            ans[i] = ans[i-1] * nums[i-1];
+            // prefix[i] = prefix[i-1] * nums[i-1];
+            // suffix[nums.length-1-i] = suffix[nums.length-i] * nums[nums.length-i];
+        }
+        
+        int r = 1;
+        for(int i = nums.length-1; i >= 0; i--){
+            ans[i] = ans[i] * r;
+            r *= nums[i];
+        }
+
+        // for(int i = nums.length-2; i >= 0; i--){
+        //     suffix[i] = suffix[i+1] * nums[i+1];
+        // }
+
+        // for(int i = 0; i < nums.length; i++){
+        //     nums[i] = prefix[i] * suffix[i];
+        // }
+
+        return ans;
+    }
+}
