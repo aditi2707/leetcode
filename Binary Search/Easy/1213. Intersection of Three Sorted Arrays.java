58fc1d59dda4c69b4b8d4a751ba604674ea5c76e
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2024-09-10 20:37:44 -0700

    Create 1213. Intersection of Three Sorted Arrays.java
    Link to the question:
    https://leetcode.com/problems/intersection-of-three-sorted-arrays/

---
+++ Binary Search/Easy/1213. Intersection of Three Sorted Arrays.java
@@ -0,0 +1,76 @@
+class Solution {
+    public List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
+        
+        List<Integer> ans = new ArrayList<>();
+        int i = 0, j = 0, k = 0;
+
+        while(i < arr1.length && j < arr2.length && k < arr3.length){
+            if(arr1[i] == arr2[j] && arr2[j] == arr3[k]){
+                ans.add(arr1[i]);
+                i++;
+                j++;
+                k++;
+                continue;
+            }
+            if(arr1[i] < arr2[j]){
+                i++;
+            }
+            else if(arr2[j] < arr3[k]){
+                j++;
+            }
+            else{
+                k++;
+            }
+        }
+
+        return ans;
+
+
+
+
+
+
+
+
+        // List<Integer> ans = new ArrayList<>();
+
+        // for(int i = 0; i < arr1.length; i++){
+        //     int num = arr1[i];
+        //     boolean found = false;
+
+        //     int low2 = 0, high2 = arr2.length-1;
+        //     while(low2 <= high2){
+        //         int mid2 = (low2 + high2) / 2;
+        //         if(arr2[mid2] == num){
+        //             found = true;
+        //             break;
+        //         }
+        //         else if(arr2[mid2] < num){
+        //             low2 = mid2 + 1;
+        //         }
+        //         else{
+        //             high2 = mid2 - 1;
+        //         }
+        //     }
+
+        //     if(found == true){
+        //         int low3 = 0, high3 = arr3.length-1;
+        //         while(low3 <= high3){
+        //             int mid3 = (low3 + high3) / 2;
+        //             if(arr3[mid3] == num){
+        //                 ans.add(num);
+        //                 break;
+        //             }
+        //             else if(arr3[mid3] < num){
+        //                 low3 = mid3 + 1;
+        //             }
+        //             else{
+        //                 high3 = mid3 - 1;
+        //             }
+        //         }
+        //     }
+        // }
+
+        // return ans;
+    }
+}
