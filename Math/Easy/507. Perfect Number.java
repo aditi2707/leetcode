711199659669b5f9769c627d7aa8df79bbcd11b8
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2024-09-15 23:17:12 -0700

    Create 507. Perfect Number.java
    Link to the question:
    https://leetcode.com/problems/perfect-number/

---
+++ Math/Easy/507. Perfect Number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        
+        int sum = 0;
+
+        int i = 1;
+
+        for(int i = 1; i < num; i++){
+            if(num % i == 0){
+                sum += i;
+            }
+        }
+
+        if(sum == num){
+            return true;
+        }
+
+        return false;
+    }
+}
