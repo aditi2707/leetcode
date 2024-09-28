c44502363e2279579097b146542433793fe29e2f
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2024-09-28 14:07:15 -0700

    Create 2006. Count Number of Pairs With Absolute Difference K.java
    Link to the question:
    https://leetcode.com/problems/count-number-of-pairs-with-absolute-difference-k/

---
+++ Hash Tables/Easy/2006. Count Number of Pairs With Absolute Difference K.java
@@ -0,0 +1,62 @@
+class Solution {
+
+    public int countKDifference(int[] nums, int k) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int ans = 0;
+
+        for(int i = 0; i < nums.length; i++){
+
+            if(map.containsKey(nums[i] - k)){
+                ans += map.get(nums[i] - k);
+            }
+            if(map.containsKey(nums[i] + k)){
+                ans += map.get(nums[i] + k);
+            }
+
+            if(!map.containsKey(nums[i])){
+                map.put(nums[i], 1);
+            }
+            else{
+                map.put(nums[i], map.get(nums[i]) + 1);
+            }
+        }
+
+        return ans;
+
+
+
+
+
+        // Arrays.sort(nums);
+        // int count = 0;
+
+        // for(int i = 0; i < nums.length; i++){
+        //     for(int j = i+1; j < nums.length; j++){
+        //         if(Math.abs(nums[i] - nums[j]) == k){
+        //             count++;
+        //         }
+        //         if(Math.abs(nums[i] - nums[j]) > k){
+        //             break;
+        //         }
+        //     }
+        // }
+
+        // return count;
+
+
+
+
+
+        
+        // for(int i = 0; i < nums.length-1; i++){
+        //     for(int j = i+1; j < nums.length; j++){
+        //         int diff = (int)(Math.abs(nums[i] - nums[j]));
+        //         if(diff == k){
+        //             counter++;
+        //         }
+        //     }
+        // }
+        // return counter;
+    }
+}
