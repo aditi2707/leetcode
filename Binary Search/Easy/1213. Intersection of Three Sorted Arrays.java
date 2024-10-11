29d1c6b38b80131e6747e14ed1a4a03f6c2ff5d4
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2024-10-11 13:22:01 -0700

    Update 1213. Intersection of Three Sorted Arrays.java
    Link to the question:
    https://leetcode.com/problems/intersection-of-three-sorted-arrays/

--- Binary Search/Easy/1213. Intersection of Three Sorted Arrays.java
+++ Binary Search/Easy/1213. Intersection of Three Sorted Arrays.java
@@ -1,29 +1,69 @@
 class Solution {
+
+    public boolean binarySearch(int[] arr, int n){
+
+        int low = 0, high = arr.length-1;
+
+        while(low <= high){
+            int mid = (low + high) / 2;
+
+            if(arr[mid] == n){
+                return true;
+            }
+            else if(arr[mid] < n){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+
+        return false;
+    }
+
     public List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
         
         List<Integer> ans = new ArrayList<>();
-        int i = 0, j = 0, k = 0;
 
-        while(i < arr1.length && j < arr2.length && k < arr3.length){
-            if(arr1[i] == arr2[j] && arr2[j] == arr3[k]){
-                ans.add(arr1[i]);
-                i++;
-                j++;
-                k++;
-                continue;
-            }
-            if(arr1[i] < arr2[j]){
-                i++;
-            }
-            else if(arr2[j] < arr3[k]){
-                j++;
-            }
-            else{
-                k++;
+        for(int i = 0; i < arr1.length; i++){
+            int n = arr1[i];
+
+            if(binarySearch(arr2, n) && binarySearch(arr3, n)){
+                ans.add(n);
             }
         }
 
         return ans;
+
+
+
+
+
+
+
+        // List<Integer> ans = new ArrayList<>();
+        // int i = 0, j = 0, k = 0;
+
+        // while(i < arr1.length && j < arr2.length && k < arr3.length){
+        //     if(arr1[i] == arr2[j] && arr2[j] == arr3[k]){
+        //         ans.add(arr1[i]);
+        //         i++;
+        //         j++;
+        //         k++;
+        //         continue;
+        //     }
+        //     if(arr1[i] < arr2[j]){
+        //         i++;
+        //     }
+        //     else if(arr2[j] < arr3[k]){
+        //         j++;
+        //     }
+        //     else{
+        //         k++;
+        //     }
+        // }
+
+        // return ans;
 
 
 
