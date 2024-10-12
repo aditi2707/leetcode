2ac2fccfce095dddc790d939972d7004e9df8c22
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2024-10-12 12:45:36 -0700

    Update 1099. Two Sum Less Than K.java
    Link to the question:
    https://leetcode.com/problems/two-sum-less-than-k/

--- Arrays/Easy/1099. Two Sum Less Than K.java
+++ Arrays/Easy/1099. Two Sum Less Than K.java
@@ -1,5 +1,48 @@
 class Solution {
+
+    public int binarySearch(int[] arr, int low, int k){
+
+        int high = arr.length - 1;
+
+        while(low <= high){
+
+            int mid = (low + high) / 2;
+
+            if(arr[mid] < k){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+
+        if(arr[low - 1] < k){
+            return low - 1;
+        }
+
+        return -1;
+    }
+
+    
     public int twoSumLessThanK(int[] nums, int k) {
+
+        // Arrays.sort(nums);
+        // int sum = -1;
+
+        // for(int i = 0; i < nums.length; i++){
+        //     int target = k - nums[i];
+
+        //     int ind = binarySearch(nums, i+1, target);
+
+        //     if(ind != -1 && ind != i){
+        //         sum = Math.max(sum, nums[i] + nums[ind]);
+        //     }
+        // }
+
+        // return sum;
+
+
+        
         
         int maxSum = -1;
 
