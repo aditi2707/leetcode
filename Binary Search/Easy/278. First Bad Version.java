42d14f83b3ac4dd34f2a2d7de1c3b521e820a71d
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2024-10-12 13:42:11 -0700

    Update 278. First Bad Version.java
    Link to the question:
    https://leetcode.com/problems/first-bad-version/

--- Binary Search/Easy/278. First Bad Version.java
+++ Binary Search/Easy/278. First Bad Version.java
@@ -3,22 +3,43 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        
+
         int low = 1, high = n;
+
         while(low <= high){
-            int mid = low + (high-low)/2;
+
+            int mid = low + (high - low) / 2;
+
             if(!isBadVersion(mid)){
-                low = mid+1;
+                low = mid + 1;
             }
             else{
-                if(!isBadVersion(mid-1)){
-                    return mid;
-                }
-                else{
-                    high = mid;
-                }
+                high = mid - 1;
             }
         }
-        return -1;
+
+        return low;
+
+
+
+
+
+        
+        // int low = 1, high = n;
+        // while(low <= high){
+        //     int mid = low + (high-low)/2;
+        //     if(!isBadVersion(mid)){
+        //         low = mid+1;
+        //     }
+        //     else{
+        //         if(!isBadVersion(mid-1)){
+        //             return mid;
+        //         }
+        //         else{
+        //             high = mid;
+        //         }
+        //     }
+        // }
+        // return -1;
     }
 }
