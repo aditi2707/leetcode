15a5500c3120b201715fc158e3d8de248d5baddb
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2024-10-11 19:30:44 -0700

    Update 1337. The K Weakest Rows in a Matrix.java
    Link to the question:
    https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/

--- Heap (Priority Queue)/Easy/1337. The K Weakest Rows in a Matrix.java
+++ Heap (Priority Queue)/Easy/1337. The K Weakest Rows in a Matrix.java
@@ -1,45 +1,96 @@
 class Solution {
 
-    public int binarySearch(int[] arr, int m, int n){
+    public int binarySearch(int[] arr){
 
-        if(m >= n){
-            return m;
+        int low = 0, high = arr.length-1;
+
+        while(low <= high){
+            int mid = (low + high) / 2;
+
+            if(arr[mid] == 1){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
         }
 
-        int mid = m + (n - m)/2;
-
-        if(arr[mid] == 1){
-            return binarySearch(arr, mid+1, n);
+        if(low < arr.length && arr[low] == 0){
+            return low;
         }
-        else{
-            return binarySearch(arr, m, mid);
-        } 
+
+        return arr.length;
     }
 
+
+
+
+    // public int binarySearch(int[] arr, int m, int n){
+
+    //     if(m >= n){
+    //         return m;
+    //     }
+
+    //     int mid = m + (n - m)/2;
+
+    //     if(arr[mid] == 1){
+    //         return binarySearch(arr, mid+1, n);
+    //     }
+    //     else{
+    //         return binarySearch(arr, m, mid);
+    //     } 
+    // }
+
     public int[] kWeakestRows(int[][] mat, int k) {
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> {
+
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> {
             return a[0] == b[0] ? a[1] - b[1] : a[0] - b[0];
         });
+
         int[] ans = new int[k];
 
         for(int i = 0; i < mat.length; i++){
-            int counter = binarySearch(mat[i], 0, mat[i].length);
-            
-            // for(int j = 0; j < mat[i].length; j++){
-            //     if(mat[i][j] == 1){
-            //         counter++;
-            //     }
-            // }
-            
-            maxHeap.add(new int[]{counter, i});
+            minHeap.add(new int[]{binarySearch(mat[i]), i});
         }
 
-        int c = 0;
-        while(c < k){
-            ans[c] = maxHeap.poll()[1];
-            c++;
+        for(int i = 0; i < k; i++){
+            ans[i] = minHeap.poll()[1];
         }
-        
+
         return ans;
+
+
+
+
+
+
+
+
+
+
+        // PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> {
+        //     return a[0] == b[0] ? a[1] - b[1] : a[0] - b[0];
+        // });
+        // int[] ans = new int[k];
+
+        // for(int i = 0; i < mat.length; i++){
+        //     int counter = binarySearch(mat[i], 0, mat[i].length);
+            
+        //     // for(int j = 0; j < mat[i].length; j++){
+        //     //     if(mat[i][j] == 1){
+        //     //         counter++;
+        //     //     }
+        //     // }
+            
+        //     maxHeap.add(new int[]{counter, i});
+        // }
+
+        // int c = 0;
+        // while(c < k){
+        //     ans[c] = maxHeap.poll()[1];
+        //     c++;
+        // }
+        
+        // return ans;
     }
 }
