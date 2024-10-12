55dd50d9a87bfa9921d18de696b52f2252ce4e9d
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2024-10-11 23:48:43 -0700

    Update 268. Missing Number.java
    Link to the question:
    https://leetcode.com/problems/missing-number/

--- Binary Search/Easy/268. Missing Number.java
+++ Binary Search/Easy/268. Missing Number.java
@@ -1,6 +1,46 @@
 class Solution {
 
+    public boolean binarySearch(int[] arr, int n){
+
+        int low = 0, high = arr.length-1;
+
+        while(low <= high){
+
+            int mid = (low + high) / 2;
+
+            if(arr[mid] == n){
+                return true;
+            }
+            else if(arr[mid] < n){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+
+        return false;
+    }
+
     public int missingNumber(int[] nums) {
+
+        // Arrays.sort(nums);
+
+        // for(int i = 0; i <= nums[nums.length-1]; i++){
+        //     if(!binarySearch(nums, i)){
+        //         return i;
+        //     }
+        // }
+
+        // return nums[nums.length-1] + 1;
+
+
+
+
+
+
+
+
         Arrays.sort(nums);
 
         int low = 0, high = nums.length-1;
@@ -16,6 +56,11 @@ class Solution {
         }
 
         return low;
+        
+
+
+
+
         
         // for(int i = 0; i < nums.length; i++){
         //     if(nums[i] != i){
