d932cbc17b8e0674567cffface2312195769b180
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2024-10-20 16:33:12 -0700

    Update 110. Balanced Binary Tree.java
    Link to the question:
    https://leetcode.com/problems/balanced-binary-tree/description/

--- Binary Trees/Easy/110. Balanced Binary Tree.java
+++ Binary Trees/Easy/110. Balanced Binary Tree.java
@@ -15,6 +15,22 @@
  */
 class Solution {
 
+    public int findBalance(TreeNode root){
+
+        if(root == null){
+            return 0;
+        }
+
+        int left = findBalance(root.left);
+        int right = findBalance(root.right);
+
+        if(Math.abs(left - right) > 1 || (left == Integer.MAX_VALUE && right == Integer.MAX_VALUE)){
+            return Integer.MAX_VALUE;
+        }
+
+        return 1 + Math.max(left, right);
+    }
+
     public int findHeight(TreeNode root, boolean[] arr){
 
         if(root == null){
@@ -39,13 +55,54 @@ class Solution {
     }
 
 
+    public int height(TreeNode root){
+
+        if(root == null){
+            return 0;
+        }
+
+        return 1 + Math.max(height(root.left), height(root.right));
+    }
+    
+
+
     public boolean isBalanced(TreeNode root) {
-        
-        boolean[] arr = {true};
-        if(findHeight(root, arr) == -1){
+
+        if(findBalance(root) == Integer.MAX_VALUE){
             return false;
         }
+
         return true;
-        //return arr[0];
+
+
+
+        // if(root == null){
+        //     return true;
+        // }
+
+        // int left = height(root.left);
+        // int right = height(root.right);
+
+        // if(Math.abs(left - right) > 1){
+        //     return false;
+        // }
+
+        // return isBalanced(root.left) && isBalanced(root.right);
+
+
+        
+
+
+
+
+
+        
+        
+        // boolean[] arr = {true};
+        // if(findHeight(root, arr) == -1){
+        //     return false;
+        // }
+        // return true;
+        // //return arr[0];
     }
 }
