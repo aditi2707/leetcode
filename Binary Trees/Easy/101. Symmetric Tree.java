2d0f86d3a5baf5fca7d848790011c13b97b411e7
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2024-10-19 23:55:49 -0700

    Update 101. Symmetric Tree.java

--- Binary Trees/Easy/101. Symmetric Tree.java
+++ Binary Trees/Easy/101. Symmetric Tree.java
@@ -21,19 +21,31 @@ class Solution {
             return true;
         }
 
-        if(root1 != null && root2 == null){
-            return false;
-        }
-        if(root2 != null && root1 == null){
+        if(root1 == null || root2 == null || root1.val != root2.val){
             return false;
         }
 
-        if(root1.val == root2.val){
-            return symSubtree(root1.left, root2.right) && symSubtree(root1.right, root2.left);
-        }
-        else{
-            return false;
-        }
+        return findSymmetry(root1.left, root2.right) && findSymmetry(root1.right, root2.left);
+
+        
+
+        // if(root1 == null && root2 == null){
+        //     return true;
+        // }
+
+        // if(root1 != null && root2 == null){
+        //     return false;
+        // }
+        // if(root2 != null && root1 == null){
+        //     return false;
+        // }
+
+        // if(root1.val == root2.val){
+        //     return symSubtree(root1.left, root2.right) && symSubtree(root1.right, root2.left);
+        // }
+        // else{
+        //     return false;
+        // }
     }
     public boolean isSymmetric(TreeNode root) {
         
