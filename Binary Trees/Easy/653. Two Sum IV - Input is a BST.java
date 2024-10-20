0063ffca32460f9b0bb5ca50e14e6b4612ee4534
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2024-10-20 12:03:52 -0700

    Update 653. Two Sum IV - Input is a BST.java
    Link to the question:
    https://leetcode.com/problems/two-sum-iv-input-is-a-bst/description/

--- Binary Trees/Easy/653. Two Sum IV - Input is a BST.java
+++ Binary Trees/Easy/653. Two Sum IV - Input is a BST.java
@@ -14,19 +14,57 @@
  * }
  */
 class Solution {
-    
-    HashSet<Integer> set = new HashSet<>();
-    public boolean findTarget(TreeNode root, int k) {
+
+    public boolean findPair(TreeNode root, int k, Set<Integer> set){
         
         if(root == null){
             return false;
         }
-        
+
         if(set.contains(k - root.val)){
             return true;
         }
         set.add(root.val);
 
-        return findTarget(root.left, k) || findTarget(root.right, k);
+        return findPair(root.left, k, set) || findPair(root.right, k, set);
+    }
+
+    public void inorder(TreeNode root, List<Integer> arr){
+
+        if(root != null){
+            inorder(root.left, arr);
+            arr.add(root.val);
+            inorder(root.right, arr);
+        }
+    }
+
+    public boolean findTarget(TreeNode root, int k) {
+
+        // Set<Integer> set = new HashSet<>();
+
+        // return findPair(root, k, set);
+
+
+
+        
+        List<Integer> arr = new ArrayList<>();
+
+        inorder(root, arr);
+
+        int i = 0, j = arr.size()-1;
+
+        while(i < j){
+            if(arr.get(i) + arr.get(j) > k){
+                j--;
+            }
+            else if(arr.get(i) + arr.get(j) < k){
+                i++;
+            }
+            else{
+                return true;
+            }
+        }
+
+        return false;
     }
 }
