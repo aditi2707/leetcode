0854a65d3399e9322f17b25bdf07b34d9d0a3d1a
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2024-10-21 19:47:14 -0700

    Update 1557. Minimum Number of Vertices to Reach All Nodes.java
    Link to the question:
    https://leetcode.com/problems/minimum-number-of-vertices-to-reach-all-nodes/

--- Graphs/Medium/1557. Minimum Number of Vertices to Reach All Nodes.java
+++ Graphs/Medium/1557. Minimum Number of Vertices to Reach All Nodes.java
@@ -1,24 +1,74 @@
 class Solution {
     public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
-        
-        List<List<Integer>> list = new ArrayList<>();
+
+        int[] inDeg = new int[n];
         List<Integer> ans = new ArrayList<>();
 
-        for(int i = 0; i < n; i++){
-            list.add(new ArrayList<>());
-        }
-
         for(int i = 0; i < edges.size(); i++){
-            
-            list.get(edges.get(i).get(1)).add(edges.get(i).get(0));
+            inDeg[edges.get(i).get(1)]++;
         }
 
         for(int i = 0; i < n; i++){
-            if(list.get(i).size() == 0){
+            if(inDeg[i] == 0){
                 ans.add(i);
             }
         }
 
         return ans;
+
+
+
+
+        // Map<Integer, Integer> inDeg = new HashMap<>();
+        // List<Integer> ans = new ArrayList<>();
+
+        // for(int i = 0; i < edges.size(); i++){
+        //     if(!inDeg.containsKey(edges.get(i).get(1))){
+        //         inDeg.put(edges.get(i).get(1), 1);
+        //     }
+        //     else{
+        //         inDeg.put(edges.get(i).get(1), inDeg.get(edges.get(i).get(1)) + 1);
+        //     }
+        //     if(!inDeg.containsKey(edges.get(i).get(0))){
+        //         inDeg.put(edges.get(i).get(0), 0);
+        //     }
+        // }
+
+        // for(Integer i: inDeg.keySet()){
+        //     if(inDeg.get(i) == 0){
+        //         ans.add(i);
+        //     }
+        // }
+
+        // return ans;
+
+
+
+
+
+        
+
+
+        
+        
+        // List<List<Integer>> list = new ArrayList<>();
+        // List<Integer> ans = new ArrayList<>();
+
+        // for(int i = 0; i < n; i++){
+        //     list.add(new ArrayList<>());
+        // }
+
+        // for(int i = 0; i < edges.size(); i++){
+            
+        //     list.get(edges.get(i).get(1)).add(edges.get(i).get(0));
+        // }
+
+        // for(int i = 0; i < n; i++){
+        //     if(list.get(i).size() == 0){
+        //         ans.add(i);
+        //     }
+        // }
+
+        // return ans;
     }
 }
