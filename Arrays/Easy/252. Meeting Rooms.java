5f2e93fea5318e2fb7d2937436f1fed5777ecf9a
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2024-10-26 20:23:20 -0700

    Update 252. Meeting Rooms.java
    Link to the question:
    https://leetcode.com/problems/meeting-rooms/

--- Arrays/Easy/252. Meeting Rooms.java
+++ Arrays/Easy/252. Meeting Rooms.java
@@ -1,21 +1,38 @@
 class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
-        
+
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
         for(int i = 1; i < intervals.length; i++){
-
-            if(intervals[i-1][1] > intervals[i][0] && intervals[i-1][1] >= intervals[i][1]){
-                return false;
-            }
-            else if(intervals[i-1][1] > intervals[i][0] && intervals[i-1][1] <= intervals[i][1]){
-                return false;
+            if(intervals[i][0] >= intervals[i-1][1]){
+                continue;
             }
             else{
-                continue;
+                return false;
             }
         }
 
         return true;
+
+
+
+        
+        
+        // Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        // for(int i = 1; i < intervals.length; i++){
+
+        //     if(intervals[i-1][1] > intervals[i][0] && intervals[i-1][1] >= intervals[i][1]){
+        //         return false;
+        //     }
+        //     else if(intervals[i-1][1] > intervals[i][0] && intervals[i-1][1] <= intervals[i][1]){
+        //         return false;
+        //     }
+        //     else{
+        //         continue;
+        //     }
+        // }
+
+        // return true;
     }
 }
