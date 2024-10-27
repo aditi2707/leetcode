c317c3289ef99196eb239a3a4f577fba4f6ca724
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2024-10-26 23:21:08 -0700

    Update 253. Meeting Rooms II.java
    Link to the question:
    https://leetcode.com/problems/meeting-rooms-ii/

--- Heap (Priority Queue)/Medium/253. Meeting Rooms II.java
+++ Heap (Priority Queue)/Medium/253. Meeting Rooms II.java
@@ -1,22 +1,58 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
 
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        
-        minHeap.add(intervals[0][1]);
+        int[] start = new int[intervals.length];
+        int[] end = new int[intervals.length];
+        int count = 0;
+        int ans = 0;
 
-        for(int i = 1; i < intervals.length; i++){
-            if(intervals[i][0] < minHeap.peek()){
-                minHeap.add(intervals[i][1]);
+        for(int i = 0; i < intervals.length; i++){
+            start[i] = intervals[i][0];
+            end[i] = intervals[i][1];
+        }
+
+        Arrays.sort(start);
+        Arrays.sort(end);
+
+        int i = 0, j = 0;
+
+        while(i < start.length){
+            if(start[i] < end[j]){
+                count++;
+                i++;
             }
             else{
-                minHeap.poll();
-                minHeap.add(intervals[i][1]);
+                count--;
+                j++;
             }
+            ans = Math.max(ans, count);
         }
+
+        return ans;
+
+
+
+
+
+
         
-        return minHeap.size();
+        
+        // PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        // Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        
+        // minHeap.add(intervals[0][1]);
+
+        // for(int i = 1; i < intervals.length; i++){
+        //     if(intervals[i][0] < minHeap.peek()){
+        //         minHeap.add(intervals[i][1]);
+        //     }
+        //     else{
+        //         minHeap.poll();
+        //         minHeap.add(intervals[i][1]);
+        //     }
+        // }
+        
+        // return minHeap.size();
     }
 }
