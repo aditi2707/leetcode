37849c582a50de6f4e6fbb17d8bcbfc95f8f59a8
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2024-10-28 11:37:05 -0700

    Update 49. Group Anagrams.java
    Link to the question:
    https://leetcode.com/problems/group-anagrams/

--- Strings/Medium/49. Group Anagrams.java
+++ Strings/Medium/49. Group Anagrams.java
@@ -1,23 +1,58 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        
+
         List<List<String>> ans = new ArrayList<>();
-        HashMap<String, List<String>> map = new HashMap<>();
+        Map<String, List<String>> map = new HashMap<>();
 
         for(int i = 0; i < strs.length; i++){
-            char[] a = strs[i].toCharArray();
-            Arrays.sort(a);
-            String b = new String(a);
-            if(!map.containsKey(b)){
-                map.put(b, new ArrayList<>());
-            }
-            map.get(b).add(strs[i]);
+            char[] c = strs[i].toCharArray();
+            Arrays.sort(c);
+            String s = new String(c);
             
+            if(!map.containsKey(s)){
+                map.put(s, new ArrayList<>());
+            }
+            map.get(s).add(strs[i]);
         }
-        
-        // for(String s: map.keySet()){
-        //     ans.add(map.get(s));
+
+        for(List<String> l: map.values()){
+            ans.add(l);
+        }
+
+        return ans;
+
+
+
+
+
+
+
+
+        // List<List<String>> ans = new ArrayList<>();
+        // Map<String, List<String>> map = new HashMap<>();
+
+        // for(int i = 0; i < strs.length; i++){
+        //     int[] arr = new int[26];
+        //     String temp = "";
+
+        //     for(int j = 0; j < strs[i].length(); j++){
+        //         arr[strs[i].charAt(j) - 'a']++;
+        //     }
+        //     for(int j = 0; j < 26; j++){
+        //         temp += "#" + String.valueOf(arr[j]);
+        //     }
+            
+        //     if(!map.containsKey(temp)){
+        //         map.put(temp, new ArrayList<>());
+        //     }
+        //     map.get(temp).add(strs[i]);
         // }
-        return new ArrayList<>(map.values());
+
+        // for(List<String> l: map.values()){
+        //     ans.add(l);
+        // }
+
+        // return ans;
+
     }
 }
