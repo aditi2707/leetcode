87952c26f7ae9bafb58f82900c5918e68082c238
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2024-10-27 18:31:56 -0700

    Update 48. Rotate Image.java
    Link to the question:
    https://leetcode.com/problems/rotate-image/

--- Matrix/Medium/48. Rotate Image.java
+++ Matrix/Medium/48. Rotate Image.java
@@ -1,8 +1,8 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        
+
         for(int i = 0; i < matrix.length; i++){
-            for(int j = i; j < matrix.length; j++){
+            for(int j = i+1; j < matrix.length; j++){
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
@@ -16,5 +16,33 @@ class Solution {
                 matrix[i][matrix.length-1-j] = temp;
             }
         }
+
+        return;
+
+
+
+
+
+
+
+
+
+        // int left = 0, right = matrix.length - 1;
+
+        // while(left < right){
+
+        //     for(int i = 0; i < right - left; i++){
+        //         int top = left, bottom = right;
+        //         int temp = matrix[top][left + i];
+        //         matrix[top][left + i] = matrix[bottom - i][left];
+        //         matrix[bottom - i][left] = matrix[bottom][right - i];
+        //         matrix[bottom][right - i] = matrix[top + i][right];
+        //         matrix[top + i][right] = temp;
+        //     }
+
+        //     left++;
+        //     right--;
+        // }
+
     }
 }
