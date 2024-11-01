faf098f45db16b341e5e11b684f4530fb1b6e060
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2024-11-01 13:53:59 -0700

    Update 167. Two Sum II - Input Array Is Sorted.java
    Link to the question:
    https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/

--- Binary Search/Medium/167. Two Sum II - Input Array Is Sorted.java
+++ Binary Search/Medium/167. Two Sum II - Input Array Is Sorted.java
@@ -1,7 +1,8 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
 
-        int i = 0, j = numbers.length-1;
+        int[] ans = new int[2];
+        int i = 0, j = numbers.length - 1;
 
         while(i < j){
             if(numbers[i] + numbers[j] > target){
@@ -11,10 +12,57 @@ class Solution {
                 i++;
             }
             else{
-                return new int[]{i+1, j+1};
+                ans[0] = i + 1;
+                ans[1] = j + 1;
+                break;
             }
         }
 
-        return new int[0];
+        return ans;
+
+
+
+
+
+
+
+
+
+        // int[] ans = new int[2];
+        // int i = 0;
+
+        // while(i < numbers.length){
+        //     int find = target - numbers[i];
+        //     int j = i + 1;
+        //     int k = numbers.length - 1;
+        //     int ind = -1;
+
+        //     while(j <= k){
+        //         int mid = j + (k - j)/2;
+
+        //         if(numbers[mid] < find){
+        //             j = mid + 1;
+        //         }
+        //         else if(numbers[mid] > find){
+        //             k = mid - 1;
+        //         }
+        //         else{
+        //             ind = mid;
+        //             break;
+        //         }
+        //     }
+
+        //     if(ind != -1){
+        //         ans[0] = i + 1;
+        //         ans[1] = ind + 1;
+        //         break;
+        //     }
+        //     else{
+        //         i++;
+        //     }
+        // }
+
+        // return ans;
+
     }
 }
