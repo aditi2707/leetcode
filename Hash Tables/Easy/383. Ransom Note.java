8289d58153c00060bc4ae258f50e3ea0c8102898
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2024-11-02 23:57:11 -0700

    Create 383. Ransom Note.java
    Link to the question:
    https://leetcode.com/problems/ransom-note/

---
+++ Hash Tables/Easy/383. Ransom Note.java
@@ -0,0 +1,60 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+        int[] freq = new int[26];
+
+        for(int i = 0; i < magazine.length(); i++){
+            char ch = magazine.charAt(i);
+            freq[ch - 'a']++;
+        }
+
+        for(int i = 0; i < ransomNote.length(); i++){
+            char ch = ransomNote.charAt(i);
+            freq[ch - 'a']--;
+
+            if(freq[ch - 'a'] < 0){
+                return false;
+            }
+        }
+
+        return true;
+
+
+
+
+
+
+
+
+        
+        // Map<Character, Integer> map = new HashMap<>();
+
+        // for(int i = 0; i < magazine.length(); i++){
+        //     char ch = magazine.charAt(i);
+
+        //     if(!map.containsKey(ch)){
+        //         map.put(ch, 1);
+        //     }
+        //     else{
+        //         map.put(ch, map.get(ch) + 1);
+        //     }
+        // }
+
+        // for(int i = 0; i < ransomNote.length(); i++){
+        //     char ch = ransomNote.charAt(i);
+
+        //     if(map.containsKey(ch)){
+        //         map.put(ch, map.get(ch) - 1);
+        //     }
+        //     else{
+        //         return false;
+        //     }
+
+        //     if(map.get(ch) == 0){
+        //         map.remove(ch);
+        //     }
+        // }
+
+        // return true;
+    }
+}
