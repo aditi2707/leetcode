66685988b42f63802c17aac607acc28f210bad7e
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2024-12-08 13:16:19 -0800

    Update 70. Climbing Stairs.java
    Link to the question:
    https://leetcode.com/problems/climbing-stairs/

--- Dynamic Programming/Easy/70. Climbing Stairs.java
+++ Dynamic Programming/Easy/70. Climbing Stairs.java
@@ -1,37 +1,82 @@
 class Solution {
 
-    // int[] dp = new int[46];
+    public int countWays(int n, int[] dp){
+
+        if(n == 1 || n == 2){
+            return n;
+        }
+
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        dp[n] = countWays(n - 1, dp) + countWays(n - 2, dp);
+        return dp[n];
+    }
+
     public int climbStairs(int n) {
 
-        int[] dp = new int[n+1];
-        dp[1] = 1;
-        if(n == 1){
-            return 1;
-        }
-        dp[2] = 2;
-        if(n == 2){
-            return 2;
+        if(n == 1 || n == 2){
+            return n;
         }
         
-        // if(n == 1){
-        //     return 1;
-            
-        // }
-        // if(n == 2){
-        //     return 2;
-        // }
-
-        // if(dp[n] != 0){
-        //     return dp[n];
-        // }
-
-        // dp[n] = climbStairs(n-1) + climbStairs(n-2);
-        // return dp[n];
+        int prev = 2;
+        int prev2 = 1;
 
         for(int i = 3; i <= n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            int curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
         }
 
-        return dp[n];
+        return prev;
+
+
+
+
+
+
+
+
+
+        // if(n == 1 || n == 2){
+        //     return n;
+        // }
+
+        // int[] dp = new int[n + 1];
+        // Arrays.fill(dp, -1);
+
+        // dp[1] = 1;
+        // dp[2] = 2;
+
+        // for(int i = 3; i <= n; i++){
+        //     dp[i] = dp[i - 1] + dp[i - 2];
+        // }
+
+        // return dp[n];
+
+
+
+
+
+
+        // int[] dp = new int[n + 1];
+        // Arrays.fill(dp, -1);
+
+        // return countWays(n, dp);
+        
+
+
+
+
+
+
+        // Time Limit Exceeded
+
+        // if(n == 1 || n == 2){
+        //     return n;
+        // }
+
+        // return climbStairs(n - 1) + climbStairs(n - 2);
     }
 }
