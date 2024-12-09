dc8991f47ae66425d5c49cc439cd52c0cba4fd12
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2024-12-09 15:33:07 -0800

    Update 62. Unique Paths.java
    Link to the question:
    https://leetcode.com/problems/unique-paths/

--- Dynamic Programming/Medium/62. Unique Paths.java
+++ Dynamic Programming/Medium/62. Unique Paths.java
@@ -43,19 +43,28 @@ class Solution {
 
 
 
-        int[][] dp = new int[m][n];
+        // int[][] dp = new int[m][n];
 
-        for(int i = 0; i < m; i++){
-            Arrays.fill(dp[i], 1);
-        }
+        // for(int i = 0; i < m; i++){
+        //     for(int j = 0; j < n; j++){
 
-        for(int i = 1; i < m; i++){
-            for(int j = 1; j < n; j++){
-                dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
-            }
-        }
+        //         if(i == 0 && j == 0){
+        //             dp[0][0] = 1;
+        //         }
+        //         else{
+        //             int up = 0, left = 0;
+        //             if(i > 0){
+        //                 up = dp[i - 1][j];
+        //             }
+        //             if(j > 0){
+        //                 left = dp[i][j - 1];
+        //             }
+        //             dp[i][j] = up + left;
+        //         }
+        //     }
+        // }
 
-        return dp[m - 1][n - 1];
+        // return dp[m - 1][n - 1];
 
 
 
