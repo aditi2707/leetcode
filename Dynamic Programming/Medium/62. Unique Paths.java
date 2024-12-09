127bd3cbf780907beb070e388661bb516a1e8ebe
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2024-12-09 13:51:41 -0800

    Update 62. Unique Paths.java
    Link to the question:
    https://leetcode.com/problems/unique-paths/

--- Dynamic Programming/Medium/62. Unique Paths.java
+++ Dynamic Programming/Medium/62. Unique Paths.java
@@ -1,32 +1,74 @@
 class Solution {
 
-    public int findPaths(int[][] dp, int m, int n){
+    public int findPaths(int i, int j, int[][] dp){
 
-        if(m == dp.length - 1 && n == dp[0].length - 1){
+        if(i < 0 || j < 0){
+            return 0;
+        }
+        if(i == 0 && j == 0){
             return 1;
         }
 
-        if(m >= dp.length || n >= dp[0].length){
-            return 0;
+        if(dp[i][j] != -1){
+            return dp[i][j];
         }
 
-        if(dp[m][n] != -1){
-            return dp[m][n];
-        }
+        int left = findPaths(i, j - 1, dp);
+        int up = findPaths(i - 1, j, dp);
 
-        dp[m][n] = findPaths(dp, m, n+1) + findPaths(dp, m+1, n);
-
-        return dp[m][n];
+        dp[i][j] = left + up;
+        return dp[i][j];
     }
 
     public int uniquePaths(int m, int n) {
-        
+
+        int[] prev = new int[n];
+        Arrays.fill(prev, 1);
+
+        for(int i = 1; i < m; i++){
+            int[] curr = new int[n];
+            Arrays.fill(curr, 1);
+
+            for(int j = 1; j < n; j++){
+                curr[j] = curr[j - 1] + prev[j];
+            }
+            prev = curr;
+        }
+
+        return prev[n - 1];
+
+
+
+
+
+
+
         int[][] dp = new int[m][n];
 
         for(int i = 0; i < m; i++){
-            Arrays.fill(dp[i], -1);
+            Arrays.fill(dp[i], 1);
         }
 
-        return findPaths(dp, 0, 0);
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+            }
+        }
+
+        return dp[m - 1][n - 1];
+
+
+
+
+
+
+        
+        // int[][] dp = new int[m][n];
+
+        // for(int i = 0; i < m; i++){
+        //     Arrays.fill(dp[i], -1);
+        // }
+
+        // return findPaths(m - 1, n - 1, dp);
     }
 }
