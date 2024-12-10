c66c860b79fc675d87c2d69a0700e26bb41c47d4
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2024-12-10 12:57:54 -0800

    Update 509. Fibonacci Number.java
    Link to the question:
    https://leetcode.com/problems/fibonacci-number/

--- Dynamic Programming/Easy/509. Fibonacci Number.java
+++ Dynamic Programming/Easy/509. Fibonacci Number.java
@@ -1,31 +1,74 @@
 class Solution {
 
-    int[] dp = new int[31];
+    public int fibonacci(int n, int[] dp){
+
+        if(n == 0 || n == 1){
+            return n;
+        }
+
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        dp[n] = fib(n - 1) + fib(n - 2);
+        return dp[n];
+    }
+
     public int fib(int n) {
 
-        // int[] dp = new int[n+1];
+        if(n == 0 || n == 1){
+            return n;
+        }
 
-        // if(n == 0){
-        //     return 0;
+        int prev2 = 0;
+        int prev = 1;
+
+        for(int i = 2; i <= n; i++){
+            int curr = prev2 + prev;
+            prev2 = prev;
+            prev = curr;
+        }
+
+        return prev;
+
+
+
+
+
+
+
+        // if(n == 0 || n == 1){
+        //     return n;
         // }
+
+        // int[] dp = new int[n + 1];
         // dp[0] = 0;
         // dp[1] = 1;
 
         // for(int i = 2; i <= n; i++){
-        //     dp[i] = dp[i-1] + dp[i-2];
+        //     dp[i] = dp[i - 1] + dp[i - 2];
         // }
 
         // return dp[n];
+
+
+
+
+
+
+        // int[] dp = new int[n + 1];
+        // Arrays.fill(dp, -1);
+
+        // return fibonacci(n, dp);
+
+
+
+
         
-        if(n == 1 || n == 0){
-            return n;
-        }
+        // if(n == 0 || n == 1){
+        //     return n;
+        // }
 
-        if(dp[n] != 0){
-            return dp[n];
-        }
-
-        dp[n] = fib(n-1) + fib(n-2);
-        return dp[n];
+        // return fib(n - 1) + fib(n - 2);
     }
 }
