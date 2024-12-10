7ef9f3047c68ae6d1e9bae0213569c1a6df3199e
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2024-12-10 13:07:22 -0800

    Update 1137. N-th Tribonacci Number.java
    Link to the question:
    https://leetcode.com/problems/n-th-tribonacci-number/

--- Dynamic Programming/Easy/1137. N-th Tribonacci Number.java
+++ Dynamic Programming/Easy/1137. N-th Tribonacci Number.java
@@ -1,41 +1,77 @@
 class Solution {
 
-    public int findTribo(int[] arr, int n){
-        if(n == 2 || n == 1){
+    public int tribo(int n, int[] dp){
+        if(n == 0){
+            return n;
+        }
+        if(n == 1 || n == 2){
             return 1;
         }
-        if(n  == 0){
-            return 0;
+
+        if(dp[n] != -1){
+            return dp[n];
         }
 
-        if(arr[n] != 0){
-            return arr[n];
-        }
-        
-        arr[n] = findTribo(arr, n-1) + findTribo(arr, n-2) + findTribo(arr, n-3);
-        return arr[n];
+        dp[n] = tribo(n - 1, dp) + tribo(n - 2, dp) + tribo(n - 3, dp);
+        return dp[n];
     }
 
     public int tribonacci(int n) {
 
-        int[] arr = new int[n+1];
-        return findTribo(arr, n);
+        if(n == 0){
+            return n;
+        }
+        if(n == 1 || n == 2){
+            return 1;
+        }
+        
+        int prev3 = 0;
+        int prev2 = 1;
+        int prev = 1;
 
-        // if(n == 2 || n == 1){
+        for(int i = 3; i <= n; i++){
+            int curr = prev + prev2 + prev3;
+            prev3 = prev2;
+            prev2 = prev;
+            prev = curr;
+        }
+
+        return prev;
+
+
+
+
+
+
+
+
+        // if(n == 0){
+        //     return n;
+        // }
+        // if(n == 1 || n == 2){
         //     return 1;
         // }
-        // if(n  == 0){
-        //     return 0;
+
+        // int[] dp = new int[n + 1];
+        // dp[0] = 0;
+        // dp[1] = 1;
+        // dp[2] = 1;
+
+        // for(int i = 3; i <= n; i++){
+        //     dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
         // }
 
-        // arr[0] = 0;
-        // arr[1] = 1;
-        // arr[2] = 1;
+        // return dp[n];
 
-        // for(int i = 3; i < arr.length; i++){
-        //     arr[i] = arr[i-1] + arr[i-2] + arr[i-3];
-        // }
 
-        //return arr[n];
+
+
+
+
+        
+        // int[] dp = new int[n + 1];
+        // Arrays.fill(dp, -1);
+
+        // return tribo(n, dp);
     }
 }
