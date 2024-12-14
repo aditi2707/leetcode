f3537a182e6b179f632d64409ed582dbacca9f9a
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2024-12-14 14:23:10 -0800

    Update 198. House Robber.java
    Link to the question:
    https://leetcode.com/problems/house-robber/description/

--- Dynamic Programming/Medium/198. House Robber.java
+++ Dynamic Programming/Medium/198. House Robber.java
@@ -1,32 +1,84 @@
 class Solution {
 
-    public int maxMoney(int[] nums, int m, int[] dp){
-
-        if(m >= nums.length){
-            return 0;
+    public int loot(int i, int[] arr, int[] dp){
+        
+        if(i == 0){
+            return arr[0];
         }
-
-        if(dp[m] != -1){
-            return dp[m];
-        }
-
-        dp[m] = Math.max(nums[m] + maxMoney(nums, m+2, dp), maxMoney(nums, m+1, dp));
-
-        return dp[m];
-    }
-
-    public int rob(int[] nums) {
-
-        if(nums.length == 1){
-            return nums[0];
-        }
-        if(nums.length == 2){
-            return Math.max(nums[0], nums[1]);
+        if(i == 1){
+            return Math.max(arr[0], arr[1]);
         }
         
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
-    
-        return maxMoney(nums, 0, dp);
+        if(dp[i] != -1){
+            return dp[i];
+        }
+        
+        int pick = arr[i] + loot(i - 2, arr, dp);
+        int notPick = loot(i - 1, arr, dp);
+        
+        dp[i] = Math.max(pick, notPick);
+        return dp[i];
+    }
+
+    public int rob(int[] arr) {
+
+        if(arr.length == 1){
+            return arr[0];
+        }
+        if(arr.length == 2){
+            return Math.max(arr[0], arr[1]);
+        }
+        
+        int prev2 = arr[0];
+        int prev1 = Math.max(arr[0], arr[1]);
+        
+        for(int i = 2; i < arr.length; i++){
+            int curr = Math.max(arr[i] + prev2, prev1);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        
+        return prev1;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // if(arr.length == 1){
+        //     return arr[0];
+        // }
+        // if(arr.length == 2){
+        //     return Math.max(arr[0], arr[1]);
+        // }
+        
+        // int[] dp = new int[arr.length];
+        
+        // dp[0] = arr[0];
+        // dp[1] = Math.max(arr[0], arr[1]);
+        
+        // for(int i = 2; i < arr.length; i++){
+        //     dp[i] = Math.max(arr[i] + dp[i - 2], dp[i - 1]);
+        // }
+        
+        // return dp[dp.length - 1];
+        
+        
+        
+        
+        
+        
+        
+        
+        // int[] dp = new int[arr.length];
+        
+        // Arrays.fill(dp, -1);
+        
+        // return Math.max(loot(arr.length - 1, arr, dp), loot(arr.length - 2, 
+        // arr, dp));
     }
 }
