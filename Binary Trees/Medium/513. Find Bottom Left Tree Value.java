10db5264f1c74b6f86445f568acb9633db60e2a1
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-01-01 15:26:09 -0800

    Update 513. Find Bottom Left Tree Value.java
    Link to the question:
    https://leetcode.com/problems/find-bottom-left-tree-value/

--- Binary Trees/Medium/513. Find Bottom Left Tree Value.java
+++ Binary Trees/Medium/513. Find Bottom Left Tree Value.java
@@ -14,31 +14,60 @@
  * }
  */
 class Solution {
-    public int findBottomLeftValue(TreeNode root) {
-        
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        int bottomLeft = root.val;
 
-        while(!queue.isEmpty()){
-            
-            bottomLeft = queue.peek().val;
-            int size = queue.size();
+    private void dfs(TreeNode root, int[] arr, int level){
 
-            while(size > 0){
-                TreeNode curr = queue.poll();
-
-                if(curr.left != null){
-                    queue.add(curr.left);
-                }
-                if(curr.right != null){
-                    queue.add(curr.right);
-                }
-
-                size--;
+        if(root == null){
+            return;
+        }
+        if(root.left == null && root.right == null){
+            if(level <= arr[0]){
+                return;
             }
+            arr[0] = level;
+            arr[1] = root.val;
+            return;
         }
 
-        return bottomLeft;
+        dfs(root.left, arr, level + 1);
+        dfs(root.right, arr, level + 1);
+
+        return;
+    }
+
+    public int findBottomLeftValue(TreeNode root) {
+
+        int[] arr = new int[2];
+        arr[1] = root.val;
+
+        dfs(root, arr, 0);
+
+        return arr[1];
+
+        
+        // Queue<TreeNode> queue = new LinkedList<>();
+        // queue.add(root);
+        // int bottomLeft = root.val;
+
+        // while(!queue.isEmpty()){
+            
+        //     bottomLeft = queue.peek().val;
+        //     int size = queue.size();
+
+        //     while(size > 0){
+        //         TreeNode curr = queue.poll();
+
+        //         if(curr.left != null){
+        //             queue.add(curr.left);
+        //         }
+        //         if(curr.right != null){
+        //             queue.add(curr.right);
+        //         }
+
+        //         size--;
+        //     }
+        // }
+
+        // return bottomLeft;
     }
 }
