0f39c20fccbc171f93960a11e074e9c7bef69418
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-01-01 13:24:28 -0800

    Create 1020. Number of Enclaves.java
    Link to the question:
    https://leetcode.com/problems/number-of-enclaves/

---
+++ Graphs/Medium/1020. Number of Enclaves.java
@@ -0,0 +1,58 @@
+class Solution {
+
+    private void dfs(int[][] grid, int[][] visited, int i, int j){
+
+        if(i < 0 || i >= grid.length){
+            return;
+        }
+        if(j < 0 || j >= grid[0].length){
+            return;
+        }
+
+        if(grid[i][j] == 0 || visited[i][j] == 1){
+            return;
+        }
+
+        visited[i][j] = 1;
+        dfs(grid, visited, i + 1, j);
+        dfs(grid, visited, i, j + 1);
+        dfs(grid, visited, i - 1, j);
+        dfs(grid, visited, i, j - 1);
+
+        return;
+    }
+
+    public int numEnclaves(int[][] grid) {
+        
+        int[][] visited = new int[grid.length][grid[0].length];
+        int enclaves = 0;
+
+        for(int j = 0; j < grid[0].length; j++){
+            if(grid[0][j] == 1){
+                dfs(grid, visited, 0, j);
+            }
+            if(grid[grid.length - 1][j] == 1){
+                dfs(grid, visited, grid.length - 1, j);
+            }
+        }
+
+        for(int i = 0; i < grid.length; i++){
+            if(grid[i][0] == 1){
+                dfs(grid, visited, i, 0);
+            }
+            if(grid[i][grid[0].length - 1] == 1){
+                dfs(grid, visited, i, grid[0].length - 1);
+            }
+        }
+
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == 1 && visited[i][j] == 0){
+                    enclaves++;
+                }
+            }
+        }
+
+        return enclaves;
+    }
+}
