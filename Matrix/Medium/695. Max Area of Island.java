6e476abbc20d4788c5d9ad8ba09ff642830b5f9a
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-01-01 19:51:29 -0800

    Update 695. Max Area of Island.java
    Link to the question:
    https://leetcode.com/problems/max-area-of-island/

--- Matrix/Medium/695. Max Area of Island.java
+++ Matrix/Medium/695. Max Area of Island.java
@@ -1,6 +1,6 @@
 class Solution {
 
-    public int dfs(int[][] grid, int i, int j){
+    private int dfs(int[][] grid, int[][] visited, int i, int j){
 
         if(i < 0 || i >= grid.length){
             return 0;
@@ -8,89 +8,34 @@ class Solution {
         if(j < 0 || j >= grid[0].length){
             return 0;
         }
-        if(grid[i][j] == 0){
+
+        if(grid[i][j] == 0 || visited[i][j] == 1){
             return 0;
         }
 
-        grid[i][j] = 0;
+        visited[i][j] = 1;
 
-        return 1 + dfs(grid, i + 1, j) + dfs(grid, i - 1, j) + 
-                    dfs(grid, i, j + 1) + dfs(grid, i, j - 1);
+        int left = dfs(grid, visited, i, j - 1);
+        int right = dfs(grid, visited, i, j + 1);
+        int up = dfs(grid, visited, i - 1, j);
+        int down = dfs(grid, visited, i + 1, j);
+
+        return 1 + left + right + up + down;
     }
-
-
-
-    public int bfs(int[][] grid, int i, int j){
-
-        int count = 1;
-        Queue<Integer> queue = new LinkedList<>();
-        int n = grid[0].length;
-
-        queue.add(i * n + j);
-
-        while(!queue.isEmpty()){
-            int curr = queue.poll();
-            int row = curr / n;
-            int col = curr % n;
-
-            if(row + 1 < grid.length && grid[row + 1][col] == 1){
-                queue.add((row + 1) * n + col);
-                grid[row + 1][col] = 0;
-                count++;
-            }
-            if(col + 1 < grid[0].length && grid[row][col + 1] == 1){
-                queue.add(row * n + (col + 1));
-                grid[row][col + 1] = 0;
-                count++;
-            }
-            if(row - 1 >= 0 && grid[row - 1][col] == 1){
-                queue.add((row - 1) * n + col);
-                grid[row - 1][col] = 0;
-                count++;
-            }
-            if(col - 1 >= 0 && grid[row][col - 1] == 1){
-                queue.add(row * n + (col - 1));
-                grid[row][col - 1] = 0;
-                count++;
-            }
-        }
-
-        return count;
-    }
-
 
     public int maxAreaOfIsland(int[][] grid) {
-
-        int area = 0;
+        
+        int maxArea = 0;
+        int[][] visited = new int[grid.length][grid[0].length];
 
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
-
-                if(grid[i][j] == 1){
-                    area = Math.max(area, dfs(grid, i, j));
+                if(grid[i][j] == 1 && visited[i][j] == 0){
+                    maxArea = Math.max(maxArea, dfs(grid, visited, i, j));
                 }
             }
         }
 
-        return area;
-
-
-
-
-
-
-        
-        // int area = 0;
-
-        // for(int i = 0; i < grid.length; i++){
-        //     for(int j = 0; j < grid[0].length; j++){
-
-        //         if(grid[i][j] == 1){
-        //             area = Math.max(area, bfs(grid, i, j));
-        //         }
-        //     }
-        // }
-
-        // return area;
+        return maxArea;
     }
 }
