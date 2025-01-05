37cfd4a648c82f16f4ada14028f16652a010f154
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-01-05 12:10:35 -0800

    Create 234. Palindrome Linked List.java
    Link to the question:
    https://leetcode.com/problems/palindrome-linked-list/

---
+++ Linked List/Easy/234. Palindrome Linked List.java
@@ -0,0 +1,105 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    private ListNode reverse(ListNode slow){
+
+        ListNode prev = null;
+        ListNode curr = slow;
+
+        while(curr != null){
+            ListNode temp = curr.next;
+            curr.next = prev;
+
+            prev = curr;
+            curr = temp;
+        }
+
+        slow = prev;
+        return slow;
+    }
+
+    public boolean isPalindrome(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        slow = slow.next;
+        ListNode curr = head;
+
+        slow = reverse(slow);
+
+        while(slow != null){
+            if(curr.val != slow.val){
+                return false;
+            }
+            curr = curr.next;
+            slow = slow.next;
+        }
+
+        return true;
+
+
+
+
+
+        // ListNode curr = head;
+        // List<Integer> list = new ArrayList<>();
+
+        // while(curr != null){
+        //     list.add(curr.val);
+        //     curr = curr.next;
+        // }
+
+        // for(int i = 0; i <= list.size()/2; i++){
+        //     if(!list.get(i).equals(list.get(list.size() - 1 - i))){
+        //         return false;
+        //     }
+        // }
+
+        // return true;
+
+
+
+
+
+
+
+        
+        // Stack<Integer> stack = new Stack<>();
+
+        // ListNode curr = head;
+
+        // while(curr != null){
+            
+        //     stack.push(curr.val);
+        //     curr = curr.next;
+        // }
+
+        // curr = head;
+        // while(curr != null){
+        //     if(curr.val == stack.peek()){
+        //         stack.pop();
+        //     }
+        //     else{
+        //         return false;
+        //     }
+        //     curr = curr.next;
+        // }
+
+        // return true;
+    }
+}
