ede74205a13424c76d6c5e0e612092481d4da010
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-01-18 14:17:46 -0800

    Update 256. Paint House.java
    Link to the question:
    https://leetcode.com/problems/paint-house/

--- Dynamic Programming/Medium/256. Paint House.java
+++ Dynamic Programming/Medium/256. Paint House.java
@@ -1,31 +1,29 @@
 class Solution {
 
-    public int paintHouse(int i, int last, int[][] costs, int[][] dp){
+    private int paintHouse(int[][] costs, int m, int n, int[][] dp){
 
-        if(i == 0){
-            int min = 1000000000;
-            for(int j = 0; j < 3; j++){
-                if(j != last){
-                    min = Math.min(min, costs[0][j]);
+        if(m == costs.length - 1){
+            int min = 2001;
+            for(int i = 0; i < 3; i++){
+                if(i != n){
+                    min = Math.min(min, costs[m][i]);
                 }
             }
-
             return min;
         }
 
-        if(dp[i][last] != -1){
-            return dp[i][last];
+        if(dp[m][n] != -1){
+            return dp[m][n];
         }
 
-        int min = 1000000000;
-        for(int j = 0; j < 3; j++){
-            if(last != j){
-                min = Math.min(min, costs[i][j] + paintHouse(i - 1, j, costs, dp));
+        int min = 2001;
+        for(int i = 0; i < 3; i++){
+            if(i != n){
+                min = Math.min(min, costs[m][i] + paintHouse(costs, m + 1, i, dp));
             }
         }
 
-        dp[i][last] = min;
-        return dp[i][last];
+        return dp[m][n] = min;
     }
 
     public int minCost(int[][] costs) {
@@ -37,23 +35,21 @@ class Solution {
         prev[2] = Math.min(costs[0][1], costs[0][0]);
         prev[3] = Math.min(costs[0][0], Math.min(costs[0][1], costs[0][2]));
 
-
         for(int i = 1; i < costs.length; i++){
             int[] curr = new int[4];
             for(int j = 0; j < 4; j++){
-                int min = 1000000000;
+                int min = 2001;
                 for(int k = 0; k < 3; k++){
                     if(k != j){
                         min = Math.min(min, costs[i][k] + prev[k]);
-                        curr[j] = min;
                     }
                 }
+                curr[j] = min;
             }
             prev = curr;
         }
 
         return prev[3];
-
 
 
 
@@ -68,16 +64,15 @@ class Solution {
         // dp[0][2] = Math.min(costs[0][1], costs[0][0]);
         // dp[0][3] = Math.min(costs[0][0], Math.min(costs[0][1], costs[0][2]));
 
-
         // for(int i = 1; i < costs.length; i++){
         //     for(int j = 0; j < 4; j++){
-        //         int min = 1000000000;
+        //         int min = 2001;
         //         for(int k = 0; k < 3; k++){
         //             if(k != j){
         //                 min = Math.min(min, costs[i][k] + dp[i - 1][k]);
-        //                 dp[i][j] = min;
         //             }
         //         }
+        //         dp[i][j] = min;
         //     }
         // }
 
@@ -85,19 +80,16 @@ class Solution {
 
 
 
-
-
-
         
-        // int min = 1000000000;
-        // int[][] dp = new int[costs.length][costs[0].length];
+        // int min = 2001;
+        // int[][] dp = new int[costs.length][3];
 
         // for(int i = 0; i < costs.length; i++){
         //     Arrays.fill(dp[i], -1);
         // }
 
-        // for(int j = 0; j < 3; j++){
-        //     min = Math.min(min, paintHouse(costs.length - 1, j, costs, dp));
+        // for(int i = 0; i < 3; i++){
+        //     min = Math.min(min, paintHouse(costs, 0, i, dp));
         // }
 
         // return min;
