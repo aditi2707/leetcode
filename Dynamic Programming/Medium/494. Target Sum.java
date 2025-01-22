c529a463fa480846ed4d039670c912454eb6ed81
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-01-22 11:05:58 -0800

    Update 494. Target Sum.java
    Link to the question:
    https://leetcode.com/problems/target-sum/description/

--- Dynamic Programming/Medium/494. Target Sum.java
+++ Dynamic Programming/Medium/494. Target Sum.java
@@ -1,14 +1,18 @@
 class Solution {
 
-    public int targetSum(int[] nums, int i, int target, int[][] dp){
+    private int targetSum(int[] nums, int i, int target, int[][] dp){
 
         if(i == 0){
-            if(nums[0] == 0 && target == 0){
+            if(target == 0 && nums[0] == 0){
                 return 2;
             }
-            if(nums[0] == target || target == 0){
+            if(target == 0 || target == nums[0]){
                 return 1;
             }
+            return 0;
+        }
+
+        if(target < 0){
             return 0;
         }
 
@@ -16,31 +20,26 @@ class Solution {
             return dp[i][target];
         }
 
+        int pick = targetSum(nums, i - 1, target - nums[i], dp);
         int notPick = targetSum(nums, i - 1, target, dp);
-        int pick = 0;
-        if(target >= nums[i]){
-            pick = targetSum(nums, i - 1, target - nums[i], dp);
-        }
 
-        dp[i][target] = pick + notPick;
-        return dp[i][target];
+        return dp[i][target] = pick + notPick;
     }
+
 
     public int findTargetSumWays(int[] nums, int target) {
 
-        int sum = 0;
+        int total = 0;
 
         for(Integer i: nums){
-            sum += i;
+            total += i;
         }
 
-        if((target + sum) % 2 != 0 || (target + sum) < 0){
+        if((total - target) % 2 != 0 || (total - target) < 0){
             return 0;
         }
 
-        int totalSum = (target + sum) / 2;
-
-        int[] prev = new int[totalSum + 1];
+        int[] prev = new int[(total - target) / 2 + 1];
 
         if(nums[0] == 0){
             prev[0] = 2;
@@ -48,46 +47,47 @@ class Solution {
         else{
             prev[0] = 1;
         }
-        
-        if(nums[0] != 0 && nums[0] <= totalSum){
-			prev[nums[0]] = 1;
-		}
+
+        if(nums[0] != 0 && nums[0] <= (total - target) / 2){
+            prev[nums[0]] = 1;
+        }
 
         for(int i = 1; i < nums.length; i++){
-            int[] curr = new int[totalSum + 1];
-            for(int j = 0; j <= totalSum; j++){
-                int notPick = prev[j];
+            int[] curr = new int[(total - target) / 2 + 1];
+            curr[0] = 1;
+            for(int j = 0; j <= (total - target) / 2; j++){
+
                 int pick = 0;
-                if(j >= nums[i]){
+
+                if(j - nums[i] >= 0){
                     pick = prev[j - nums[i]];
                 }
+
+                int notPick = prev[j];
 
                 curr[j] = pick + notPick;
             }
             prev = curr;
         }
 
-        return prev[totalSum];
+        return prev[(total - target) / 2];
 
 
 
 
 
 
-
-        // int sum = 0;
+        // int total = 0;
 
         // for(Integer i: nums){
-        //     sum += i;
+        //     total += i;
         // }
 
-        // if((target + sum) % 2 != 0 || (target + sum) < 0){
+        // if((total - target) % 2 != 0 || (total - target) < 0){
         //     return 0;
         // }
 
-        // int totalSum = (target + sum) / 2;
-
-        // int[][] dp = new int[nums.length][totalSum + 1];
+        // int[][] dp = new int[nums.length][(total - target) / 2 + 1];
 
         // if(nums[0] == 0){
         //     dp[0][0] = 2;
@@ -95,67 +95,63 @@ class Solution {
         // else{
         //     dp[0][0] = 1;
         // }
-        
-        // if(nums[0] != 0 && nums[0] <= totalSum){
-		// 	dp[0][nums[0]] = 1;
-		// }
+
+        // if(nums[0] != 0 && nums[0] <= (total - target) / 2){
+        //     dp[0][nums[0]] = 1;
+        // }
 
         // for(int i = 1; i < nums.length; i++){
-        //     for(int j = 0; j <= totalSum; j++){
-        //         int notPick = dp[i - 1][j];
+        //     for(int j = 0; j <= (total - target) / 2; j++){
+
         //         int pick = 0;
-        //         if(j >= nums[i]){
+
+        //         if(j - nums[i] >= 0){
         //             pick = dp[i - 1][j - nums[i]];
         //         }
+
+        //         int notPick = dp[i - 1][j];
 
         //         dp[i][j] = pick + notPick;
         //     }
         // }
 
-        // return dp[nums.length - 1][totalSum];
+        // return dp[nums.length - 1][(total - target) / 2];
 
 
 
 
 
-
-        // int sum = 0;
+        // int total = 0;
 
         // for(Integer i: nums){
-        //     sum += i;
+        //     total += i;
         // }
 
-        // if((target + sum) % 2 != 0 || (target + sum) < 0){
+        // if((total - target) % 2 != 0 || (total - target) < 0){
         //     return 0;
         // }
 
-        // int totalSum = (target + sum) / 2;
-
-        // int[][] dp = new int[nums.length][totalSum + 1];
+        // int[][] dp = new int[nums.length][(total - target) / 2 + 1];
 
         // for(int i = 0; i < nums.length; i++){
         //     Arrays.fill(dp[i], -1);
         // }
 
-        // return targetSum(nums, nums.length - 1, totalSum, dp);
+        // return targetSum(nums, nums.length - 1, (total - target) / 2, dp);
 
 
 
-
-
-
-        // int sum = 0;
+        
+        // int total = 0;
 
         // for(Integer i: nums){
-        //     sum += i;
+        //     total += i;
         // }
 
-        // if((target + sum) % 2 != 0 || (target + sum) < 0){
+        // if((total - target) % 2 != 0){
         //     return 0;
         // }
 
-        // int totalSum = (target + sum) / 2;
-
-        // return targetSum(nums, nums.length - 1, totalSum);
+        // return targetSum(nums, nums.length - 1, (total - target) / 2);
     }
 }
