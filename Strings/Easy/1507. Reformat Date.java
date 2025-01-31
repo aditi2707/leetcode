b0ca1833d56ebbbc3b977f9826adc204f0c748d7
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-01-30 20:07:12 -0800

    Update 1507. Reformat Date.java
    Link to the question:
    https://leetcode.com/problems/reformat-date/

--- Strings/Easy/1507. Reformat Date.java
+++ Strings/Easy/1507. Reformat Date.java
@@ -1,36 +1,73 @@
 class Solution {
     public String reformatDate(String date) {
-        
-        //HashMap<String, String> month = new HashMap<>();
-        String finalDate = "";
-        String[] dateParts = date.split(" ");
-        String[] month = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
-        String dpDate = dateParts[0];
-        String dpMonth = dateParts[1];
-        String dpYear = dateParts[2];
 
-        finalDate = dpYear + "-";
+        Map<String, String> map = new HashMap<>();
 
-        for(int i = 0; i < 12; i++){
-            if(month[i].equals(dpMonth) && i+1 < 10){
+        map.put("Jan", "01");
+        map.put("Feb", "02");
+        map.put("Mar", "03");
+        map.put("Apr", "04");
+        map.put("May", "05");
+        map.put("Jun", "06");
+        map.put("Jul", "07");
+        map.put("Aug", "08");
+        map.put("Sep", "09");
+        map.put("Oct", "10");
+        map.put("Nov", "11");
+        map.put("Dec", "12");
 
-                finalDate += "0" + Integer.toString(i+1);
-                break;
-            }
-            if(month[i].equals(dpMonth) && i+1 >= 10){
-                finalDate += Integer.toString(i+1);
-                break;
-            }
-        }
+        String year = date.substring(date.length() - 4);
+        String extractMonth = date.substring(date.indexOf(" ") + 1, 
+        date.indexOf(" ") + 4);
+        String month = map.get(extractMonth);
 
-        if(dpDate.length() == 4){
-            finalDate += "-" + dpDate.substring(0, 2);
+        String day = "";
+        if(!Character.isDigit(date.charAt(1))){
+            day = "0" + date.charAt(0);
         }
         else{
-            finalDate += "-0" + dpDate.substring(0, 1);
+            day = date.substring(0, 2);
         }
 
-        return finalDate;
+        return year + "-" + month + "-" + day;
+
+
+
+        
+        
+        // //HashMap<String, String> month = new HashMap<>();
+        // String finalDate = "";
+        // String[] dateParts = date.split(" ");
+        // String[] month = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+        // String dpDate = dateParts[0];
+        // String dpMonth = dateParts[1];
+        // String dpYear = dateParts[2];
+
+        // finalDate = dpYear + "-";
+
+        // for(int i = 0; i < 12; i++){
+        //     if(month[i].equals(dpMonth) && i+1 < 10){
+
+        //         finalDate += "0" + Integer.toString(i+1);
+        //         break;
+        //     }
+        //     if(month[i].equals(dpMonth) && i+1 >= 10){
+        //         finalDate += Integer.toString(i+1);
+        //         break;
+        //     }
+        // }
+
+        // if(dpDate.length() == 4){
+        //     finalDate += "-" + dpDate.substring(0, 2);
+        // }
+        // else{
+        //     finalDate += "-0" + dpDate.substring(0, 1);
+        // }
+
+        // return finalDate;
+
+
+
 
         // month.put("Jan", "01");
         // month.put("Feb", "02");
