7b374b7c131b2c9f3f4699577787f3e57f96def7
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-01-31 13:20:36 -0800

    Update 46. Permutations.java
    Link to the question:
    https://leetcode.com/problems/permutations/

--- Backtracking/Medium/46. Permutations.java
+++ Backtracking/Medium/46. Permutations.java
@@ -1,4 +1,29 @@
+// Time Complexity : O(n * n!)
+// Because the number of permutations for each value will be n!. For n values, it would be n * n!.
+// Space Complexity : O(n + n^2)
+// If the output array is ignored, then array temp is being used which has a space complexity of O(n).
+
 class Solution {
+
+    private void backtracking(int[] nums, int start, List<Integer> temp, 
+    List<List<Integer>> ans){
+
+        if(temp.size() == nums.length){
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for(int i = start; i < nums.length; i++){
+            if(!temp.contains(nums[i])){
+                temp.add(nums[i]);
+                backtracking(nums, 0, temp, ans);
+                temp.remove(temp.size() - 1);
+            }
+        }
+
+        return;
+    }
+    
 
     public void findPermute(int[] nums, int start, List<List<Integer>> ans){
         
@@ -25,10 +50,19 @@ class Solution {
     }
 
     public List<List<Integer>> permute(int[] nums) {
-        
+
         List<List<Integer>> ans = new ArrayList<>();
-        findPermute(nums, 0, ans);
+        List<Integer> temp = new ArrayList<>();
+
+        backtracking(nums, 0, temp, ans);
 
         return ans;
+
+        
+        
+        // List<List<Integer>> ans = new ArrayList<>();
+        // findPermute(nums, 0, ans);
+
+        // return ans;
     }
 }
