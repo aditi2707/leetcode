ffbb859206ccf793a821ad9f0100d3618f8030e4
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-01-30 19:54:02 -0800

    Update 1086. High Five.java
    Link to the question:
    https://leetcode.com/problems/high-five/

--- Heap (Priority Queue)/Easy/1086. High Five.java
+++ Heap (Priority Queue)/Easy/1086. High Five.java
@@ -1,52 +1,69 @@
 class Solution {
     public int[][] highFive(int[][] items) {
-        
-        HashMap<Integer, PriorityQueue<Integer>> map = new HashMap<>();
-        
+
+        Map<Integer, PriorityQueue<Integer>> map = new HashMap<>();
+
         for(int i = 0; i < items.length; i++){
             if(!map.containsKey(items[i][0])){
                 map.put(items[i][0], new PriorityQueue<>());
-                //map.put(items[i][0], new ArrayList<>());
             }
             map.get(items[i][0]).add(items[i][1]);
+
             if(map.get(items[i][0]).size() > 5){
                 map.get(items[i][0]).poll();
             }
         }
 
-        int[][] result = new int[map.size()][2];
-        int k = 0;
+        int[][] ans = new int[map.size()][2];
+        List<Integer> list = new ArrayList<>(map.keySet());
 
-        for(Integer i: map.keySet()){
-            int sum = map.get(i).poll() + map.get(i).poll() + map.get(i).poll() + map.get(i).poll() + map.get(i).poll();
-            int average = sum/5;
-            result[k][0] = i;
-            result[k][1] = average;
-            k++;
+        Collections.sort(list);
+
+        for(int i = 0; i < list.size(); i++){
+            int sum = 0;
+            int id = list.get(i);
+            while(map.get(id).size() > 0){
+                sum += map.get(id).poll();
+            }
+            ans[i][0] = id;
+            ans[i][1] = sum / 5;
         }
 
+        return ans;
+
+
+
+
+        
+        // Map<Integer, PriorityQueue<Integer>> map = new HashMap<>();
+
+        // for(int i = 0; i < items.length; i++){
+        //     if(!map.containsKey(items[i][0])){
+        //         map.put(items[i][0], new PriorityQueue<>());
+        //     }
+        //     map.get(items[i][0]).add(items[i][1]);
+
+        //     if(map.get(items[i][0]).size() > 5){
+        //         map.get(items[i][0]).poll();
+        //     }
+        // }
+
+        // int[][] ans = new int[map.size()][2];
         // int k = 0;
+
         // for(Integer i: map.keySet()){
-        //     int s = 0;
-        //     PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        //     while(s < map.get(i).size()){
-        //         minHeap.add(map.get(i).get(s));
-        //         if(minHeap.size() > 5){
-        //             minHeap.poll();
-        //         }
-        //         s++;
-        //     }
         //     int sum = 0;
-        //     while(minHeap.size() > 0){
-        //         sum += minHeap.poll();
+        //     while(map.get(i).size() > 0){
+        //         sum += map.get(i).poll();
         //     }
-        //     int average = sum/5;
-        //     result[k][0] = i;
-        //     result[k][1] = average;
+        //     ans[k][0] = i;
+        //     ans[k][1] = sum / 5;
+
         //     k++;
         // }
 
-        Arrays.sort(result, (a, b) -> Integer.compare(a[0], b[0]));
-        return result;
+        // Arrays.sort(ans, (a, b) -> Integer.compare(a[0], b[0]));
+
+        // return ans;
     }
 }
