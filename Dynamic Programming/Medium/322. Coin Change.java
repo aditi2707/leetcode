0bf0bf997a474e262d1067e117b0b5acb79a1f72
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-01-30 19:33:30 -0800

    Update 322. Coin Change.java
    Link to the question:
    https://leetcode.com/problems/coin-change/

--- Dynamic Programming/Medium/322. Coin Change.java
+++ Dynamic Programming/Medium/322. Coin Change.java
@@ -1,49 +1,88 @@
 class Solution {
 
-    public int unboundedKnapsack(int[] coins, int m, int amount, int[][] dp){
+    private int coinChange(int[] coins, int i, int amount, int[][] dp){
 
-        if(amount == 0){
-            return 0;
-        }
-        if(m < 0){
-            return Integer.MAX_VALUE - 1;
-        }
-        if(m == 0){
-            if(amount % coins[m] == 0){
-                return amount / coins[m];
+        if(i == 0){
+            if(amount % coins[0] == 0){
+                return amount / coins[0];
             }
-            else{
-                return Integer.MAX_VALUE - 1;
-            }
+            return 1000000000;
         }
 
-        if(dp[m][amount] != -1){
-            return dp[m][amount];
+        if(dp[i][amount] != -1){
+            return dp[i][amount];
         }
 
-        if(amount - coins[m] >= 0){
-            dp[m][amount] = Math.min(unboundedKnapsack(coins, m, amount - coins[m], dp) + 1, unboundedKnapsack(coins, m-1, amount, dp));
+        if(amount - coins[i] >= 0){
+            dp[i][amount] = 
+            Math.min(1 + coinChange(coins, i, amount - coins[i], dp), 
+            coinChange(coins, i - 1, amount, dp));
         }
         else{
-            dp[m][amount] = unboundedKnapsack(coins, m-1, amount, dp);
+            dp[i][amount] = coinChange(coins, i - 1, amount, dp);
         }
 
-        return dp[m][amount];
+        return dp[i][amount];
     }
+
 
     public int coinChange(int[] coins, int amount) {
 
-        int[][] dp = new int[coins.length + 1][amount + 1];
+        int[] prev = new int[amount + 1];
 
-        for(int i = 0; i <= coins.length; i++){
-            Arrays.fill(dp[i], -1);
+        for(int i = 1; i <= amount; i++){
+            prev[i] = 1000000000;
         }
+
+        for(int i = 1; i <= coins.length; i++){
+            int[] curr = new int[amount + 1];
+            for(int j = 1; j <= amount; j++){
+                if(j - coins[i - 1] >= 0){
+                    curr[j] = Math.min(1 + curr[j - coins[i - 1]], prev[j]);
+                }
+                else{
+                    curr[j] = prev[j];
+                }
+            }
+            prev = curr;
+        }
+
+        return prev[amount] >= 1000000000? -1 : prev[amount];
+
+
+
+
+
+        // int[][] dp = new int[coins.length + 1][amount + 1];
+
+        // for(int i = 1; i <= amount; i++){
+        //     dp[0][i] = 1000000000;
+        // }
+
+        // for(int i = 1; i <= coins.length; i++){
+        //     for(int j = 1; j <= amount; j++){
+        //         if(j - coins[i - 1] >= 0){
+        //             dp[i][j] = Math.min(1 + dp[i][j - coins[i - 1]], dp[i - 1][j]);
+        //         }
+        //         else{
+        //             dp[i][j] = dp[i - 1][j];
+        //         }
+        //     }
+        // }
+
+        // return dp[coins.length][amount] >= 1000000000? -1 : dp[coins.length][amount];
+
+
+
+
+
+        // int[][] dp = new int[coins.length][amount + 1];
+
+        // for(int i = 0; i < coins.length; i++){
+        //     Arrays.fill(dp[i], -1);
+        // }
         
-        int ans = unboundedKnapsack(coins, coins.length-1, amount, dp);
-        if(ans == Integer.MAX_VALUE - 1){
-            return -1;
-        }
-
-        return ans;
+        // int ans = coinChange(coins, coins.length - 1, amount, dp);
+        // return ans >= 1000000000? -1 : ans;
     }
 }
