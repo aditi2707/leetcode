1aa5625820f2e2909a6c36a77c55c61153b809f5
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-01-31 12:19:09 -0800

    Update 1823. Find the Winner of the Circular Game.java
    Link to the question:
    https://leetcode.com/problems/find-the-winner-of-the-circular-game/

--- Arrays/Medium/1823. Find the Winner of the Circular Game.java
+++ Arrays/Medium/1823. Find the Winner of the Circular Game.java
@@ -2,39 +2,37 @@ class Solution {
     public int findTheWinner(int n, int k) {
 
         List<Integer> list = new ArrayList<>();
-        int curr = 0;
 
         for(int i = 1; i <= n; i++){
             list.add(i);
         }
 
+        int c = 0;
         while(list.size() > 1){
-            curr = (curr+k-1)%list.size();
-            list.remove(curr);
+            c = (c + k - 1) % list.size();
+            list.remove(c);
         }
 
         return list.get(0);
+
+
+
         
         // Queue<Integer> queue = new LinkedList<>();
-        // int c = 1;
-
-        // if(n == 1){
-        //     return 1;
-        // }
+        // int c = k;
 
         // for(int i = 1; i <= n; i++){
         //     queue.add(i);
         // }
 
-        // while(queue.size() != 1){
-
-        //     if(c < k){
-        //         queue.add(queue.poll());
-        //         c++;
+        // while(queue.size() > 1){
+        //     int poll = queue.poll();
+        //     if(c != 1){
+        //         queue.add(poll);
+        //         c--;
         //     }
-        //     else if(c == k){
-        //         queue.poll();
-        //         c = 1;
+        //     else{
+        //         c = k;
         //     }
         // }
 
