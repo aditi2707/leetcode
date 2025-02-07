a672f844c48309cd7ba604aa57a279382186d0b4
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-02-07 10:43:15 -0800

    Update 1683. Invalid Tweets.sql
    Link to the question:
    https://leetcode.com/problems/invalid-tweets/

--- Database/Easy/1683. Invalid Tweets.sql
+++ Database/Easy/1683. Invalid Tweets.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
-select tweet_id
-from Tweets
-where LENGTH(content) > 15
+-- Write your MySQL query statement below
+SELECT tweet_id
+FROM Tweets
+WHERE LENGTH(content) > 15
