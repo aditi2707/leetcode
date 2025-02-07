54a61c3bde79efb62836ff987818a5ede21b30cb
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-02-07 10:56:03 -0800

    Update 1068. Product Sales Analysis I.sql
    Link to the question:
    https://leetcode.com/problems/product-sales-analysis-i/description/

--- Database/Easy/1068. Product Sales Analysis I.sql
+++ Database/Easy/1068. Product Sales Analysis I.sql
@@ -1,3 +1,4 @@
-# Write your MySQL query statement below
-select p.product_name, s.year, s.price
-from Product as p join Sales as s on s.product_id = p.product_id
+-- Write your MySQL query statement below
+SELECT p.product_name, s.year, s.price
+FROM Product AS p JOIN Sales AS s
+ON p.product_id = s.product_id
