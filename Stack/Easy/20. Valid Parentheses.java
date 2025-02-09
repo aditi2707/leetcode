56b81c5329d18c653fcee074407c5cc0f5b9feda
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-02-09 12:19:39 -0800

    Update 20. Valid Parentheses.java
    Link to the question:
    https://leetcode.com/problems/valid-parentheses/

--- Stack/Easy/20. Valid Parentheses.java
+++ Stack/Easy/20. Valid Parentheses.java
@@ -2,32 +2,37 @@ class Solution {
     public boolean isValid(String s) {
         
         Stack<Character> stack = new Stack<>();
+        int i = 1;
 
-        for(int i = 0; i < s.length(); i++){
+        stack.push(s.charAt(0));
+
+        while(i < s.length()){
             char ch = s.charAt(i);
 
-            if(ch == '(' || ch == '{' || ch == '['){
+            if(ch == '(' || ch == '[' || ch == '{'){
                 stack.push(ch);
             }
             else{
-                if(!stack.isEmpty()){
-                    if(ch == ']' && stack.peek() == '['){
+                if(stack.isEmpty()){
+                    return false;
+                }
+                else{
+                    char peek = stack.peek();
+                    if(ch == ')' && peek == '('){
                         stack.pop();
                     }
-                    else if(ch == ')' && stack.peek() == '('){
+                    else if(ch == ']' && peek == '['){
                         stack.pop();
                     }
-                    else if(ch == '}' && stack.peek() == '{'){
+                    else if(ch == '}' && peek == '{'){
                         stack.pop();
                     }
                     else{
-                        stack.push(ch);
+                        return false;
                     }
                 }
-                else{
-                    return false;
-                }
             }
+            i++;
         }
 
         return stack.isEmpty();
