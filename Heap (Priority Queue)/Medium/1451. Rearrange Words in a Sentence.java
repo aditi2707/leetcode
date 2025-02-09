9e38a60a04a048e06b74a9f763c3b891524c6efa
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-02-09 14:01:50 -0800

    Create 1451. Rearrange Words in a Sentence.java
    Link to the question:
    https://leetcode.com/problems/rearrange-words-in-a-sentence/

---
+++ Heap (Priority Queue)/Medium/1451. Rearrange Words in a Sentence.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String arrangeWords(String text) {
+
+        // Time Complexity : O(nlog n + n)
+
+        // Space Complexity : O(2 * n)
+        
+        
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> {
+            return a[0] == b[0] ? a[1] - b[1] : a[0] - b[0];
+        });
+
+        String ans = "";
+        String[] arr = text.split(" ");
+
+        arr[0] = Character.toLowerCase(arr[0].charAt(0)) + arr[0].substring(1);
+
+        for(int i = 0; i < arr.length; i++){
+            String s = arr[i];
+            minHeap.add(new int[]{s.length(), i});
+        }
+
+        while(minHeap.size() > 0){
+            ans += arr[minHeap.poll()[1]] + " ";
+        }
+
+        ans = Character.toUpperCase(ans.charAt(0)) + ans.substring(1, ans.length() - 1);
+
+        return ans;
+    }
+}
