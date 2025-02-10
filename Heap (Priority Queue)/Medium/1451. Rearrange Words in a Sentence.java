2a7c89b13706d226ef74764655f60d0fa740edfc
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-02-09 18:56:13 -0800

    Update 1451. Rearrange Words in a Sentence.java
    Link to the question:
    https://leetcode.com/problems/rearrange-words-in-a-sentence/

--- Heap (Priority Queue)/Medium/1451. Rearrange Words in a Sentence.java
+++ Heap (Priority Queue)/Medium/1451. Rearrange Words in a Sentence.java
@@ -3,29 +3,51 @@ class Solution {
 
         // Time Complexity : O(nlog n + n)
 
-        // Space Complexity : O(2 * n)
-        
-        
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> {
-            return a[0] == b[0] ? a[1] - b[1] : a[0] - b[0];
-        });
+        // Space Complexity : O(n)
+
 
         String ans = "";
-        String[] arr = text.split(" ");
+        String[] arr = text.toLowerCase().split(" ");
 
-        arr[0] = Character.toLowerCase(arr[0].charAt(0)) + arr[0].substring(1);
+        Arrays.sort(arr, (a, b) -> a.length() - b.length());
 
         for(int i = 0; i < arr.length; i++){
-            String s = arr[i];
-            minHeap.add(new int[]{s.length(), i});
-        }
-
-        while(minHeap.size() > 0){
-            ans += arr[minHeap.poll()[1]] + " ";
+            ans += arr[i] + " ";
         }
 
         ans = Character.toUpperCase(ans.charAt(0)) + ans.substring(1, ans.length() - 1);
 
         return ans;
+
+
+
+        
+
+        // // Time Complexity : O(nlog n + n)
+
+        // // Space Complexity : O(2 * n)
+        
+        
+        // PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> {
+        //     return a[0] == b[0] ? a[1] - b[1] : a[0] - b[0];
+        // });
+
+        // String ans = "";
+        // String[] arr = text.split(" ");
+
+        // arr[0] = Character.toLowerCase(arr[0].charAt(0)) + arr[0].substring(1);
+
+        // for(int i = 0; i < arr.length; i++){
+        //     String s = arr[i];
+        //     minHeap.add(new int[]{s.length(), i});
+        // }
+
+        // while(minHeap.size() > 0){
+        //     ans += arr[minHeap.poll()[1]] + " ";
+        // }
+
+        // ans = Character.toUpperCase(ans.charAt(0)) + ans.substring(1, ans.length() - 1);
+
+        // return ans;
     }
 }
