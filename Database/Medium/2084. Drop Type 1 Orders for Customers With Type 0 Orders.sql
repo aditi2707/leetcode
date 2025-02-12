ac2cf2416d6843f273472d8b4b88eca5d575ef84
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-02-12 11:47:18 -0800

    Create 2084. Drop Type 1 Orders for Customers With Type 0 Orders.sql
    Link to the question:
    https://leetcode.com/problems/drop-type-1-orders-for-customers-with-type-0-orders/

---
+++ Database/Medium/2084. Drop Type 1 Orders for Customers With Type 0 Orders.sql
@@ -0,0 +1,9 @@
+-- Write your MySQL query statement below
+SELECT order_id, customer_id, order_type
+FROM Orders
+WHERE (customer_id NOT IN (
+    SELECT DISTINCT customer_id
+    FROM Orders
+    WHERE order_type = 0
+))
+OR order_type = 0
