7cd48dfb9cdb75322c8dbb6c96afb2efdc3255e7
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-02-17 12:53:24 -0800

    Update 131. Palindrome Partitioning.java
    Link to the question:
    https://leetcode.com/problems/palindrome-partitioning/

--- Backtracking/Medium/131. Palindrome Partitioning.java
+++ Backtracking/Medium/131. Palindrome Partitioning.java
@@ -1,40 +1,77 @@
 class Solution {
 
-    public boolean isPalindrome(String s){
+    private boolean isPalindrome(String s){
 
-        for(int i = 0; i < s.length()/2; i++){
-            if(s.charAt(i) == s.charAt(s.length()-1-i)){
-                continue;
-            }
-            else{
+        int i = 0, j = s.length() - 1;
+
+        while(i < j){
+            if(s.charAt(i) != s.charAt(j)){
                 return false;
             }
+            i++;
+            j--;
         }
 
         return true;
     }
 
-    public void dfs(List<List<String>> ans, List<String> temp, int start, String s){
+    private void backtracking(String s, List<List<String>> ans, List<String> temp,  
+    int start, boolean[][] dp){
 
-        if(start >= s.length()){
-            ans.add(new ArrayList<String>(temp));
+        if(start == s.length()){
+            ans.add(new ArrayList<>(temp));
+            return;
         }
 
-        for(int i = start; i < s.length(); i++){
-            if(isPalindrome(s.substring(start, i+1))){
-                temp.add(s.substring(start, i+1));
-                dfs(ans, temp, i+1, s);
-                temp.remove(temp.size()-1);
+        for(int end = start; end < s.length(); end++){
+            if(s.charAt(start) == s.charAt(end) && 
+            ((end - start <= 2) || dp[start + 1][end - 1])){
+                dp[start][end] = true;
+                temp.add(s.substring(start, end + 1));
+                backtracking(s, ans, temp, end + 1, dp);
+                temp.remove(temp.size() - 1);
             }
         }
+
+        return;
     }
 
+
+    private void backtracking(String s, List<List<String>> ans, List<String> temp,  
+    int ind){
+
+        if(ind == s.length()){
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for(int i = ind; i < s.length(); i++){
+            if(isPalindrome(s.substring(ind, i + 1))){
+                temp.add(s.substring(ind, i + 1));
+                backtracking(s, ans, temp, i + 1);
+                temp.remove(temp.size() - 1);
+            }
+        }
+
+        return;
+    }
+
+
     public List<List<String>> partition(String s) {
-        
+
+        boolean[][] dp = new boolean[s.length()][s.length()];
         List<List<String>> ans = new ArrayList<>();
-        
-        dfs(ans, new ArrayList<>(), 0, s);
+
+        backtracking(s, ans, new ArrayList<>(), 0, dp);
 
         return ans;
+
+
+    
+        // List<List<String>> ans = new ArrayList<>();
+
+        // backtracking(s, ans, new ArrayList<>(), 0);
+
+        // return ans;
     }
 }
