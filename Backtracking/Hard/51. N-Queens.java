19a4563ca96678ce306d15272351264ca15fb5d3
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-03-08 13:50:16 -0800

    Update 51. N-Queens.java
    Link to the question:
    https://leetcode.com/problems/n-queens/

--- Backtracking/Hard/51. N-Queens.java
+++ Backtracking/Hard/51. N-Queens.java
@@ -1,54 +1,55 @@
 class Solution {
 
-    Set<Integer> cols = new HashSet<>();
-    Set<Integer> posDiag = new HashSet<>();
-    Set<Integer> negDiag = new HashSet<>();
+    private Set<Integer> cols = new HashSet<>();
+    private Set<Integer> posDiag = new HashSet<>();
+    private Set<Integer> negDiag = new HashSet<>();
 
-    public void backtracking(int k, List<List<String>> ans, char[][] temp, int m){
+    private void backtracking(List<List<String>> ans, char[][] board, int m, int n){
 
-        if(m == k){
-            List<String> t = new ArrayList<>();
-            for(int i = 0; i < k; i++){
-                String str = new String(temp[i]);
-                t.add(str);
+        if(m == n){
+            List<String> temp = new ArrayList<>();
+            for(int i = 0; i < n; i++){
+                String str = new String(board[i]);
+                temp.add(str);
             }
-            ans.add(t);
+            ans.add(temp);
             return;
         }
 
-        for(int c = 0; c < k; c++){
-            if(cols.contains(c) || posDiag.contains(m + c) || negDiag.contains(m - c)){
+        for(int j = 0; j < n; j++){
+            if(cols.contains(j) || posDiag.contains(m - j) || 
+            negDiag.contains(m + j)){
                 continue;
             }
 
-            cols.add(c);
-            posDiag.add(m + c);
-            negDiag.add(m - c);
+            cols.add(j);
+            posDiag.add(m - j);
+            negDiag.add(m + j);
 
-            temp[m][c] = 'Q';
+            board[m][j] = 'Q';
 
-            backtracking(k, ans, temp, m + 1);
+            backtracking(ans, board, m + 1, n);
 
-            cols.remove(c);
-            posDiag.remove(m + c);
-            negDiag.remove(m - c);
+            cols.remove(j);
+            posDiag.remove(m - j);
+            negDiag.remove(m + j);
 
-            temp[m][c] = '.';
+            board[m][j] = '.';
         }
-    }
 
+        return;
+    }
 
     public List<List<String>> solveNQueens(int n) {
         
         List<List<String>> ans = new ArrayList<>();
-        
-        char[][] temp = new char[n][n];
+        char[][] board = new char[n][n];
 
         for(int i = 0; i < n; i++){
-            Arrays.fill(temp[i], '.');
+            Arrays.fill(board[i], '.');
         }
-        
-        backtracking(n, ans, temp, 0);
+
+        backtracking(ans, board, 0, n);
 
         return ans;
     }
