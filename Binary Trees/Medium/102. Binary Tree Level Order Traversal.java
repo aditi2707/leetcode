a0831923ce0301411fd6e14976660c6d1c49d9b7
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-03-25 20:24:45 -0700

    Update 102. Binary Tree Level Order Traversal.java
    Link to the question:
    https://leetcode.com/problems/binary-tree-level-order-traversal/

--- Binary Trees/Medium/102. Binary Tree Level Order Traversal.java
+++ Binary Trees/Medium/102. Binary Tree Level Order Traversal.java
@@ -15,64 +15,86 @@
  */
 class Solution {
 
-    public void levelOrderTraversal(TreeNode root, int level, List<List<Integer>> list){
+    private void levelorderDFS(TreeNode root, List<List<Integer>> ans, int level){
 
         if(root == null){
             return;
         }
 
-        if(level == list.size()){
-            list.add(new ArrayList<>());
+        if(ans.size() == level){
+            ans.add(new ArrayList<>());
         }
-        
-        list.get(level).add(root.val);
-        levelOrderTraversal(root.left, level+1, list);
-        levelOrderTraversal(root.right, level+1, list);
+        ans.get(level).add(root.val);
+
+        levelorderDFS(root.left, ans, level + 1);
+        levelorderDFS(root.right, ans, level + 1);
+
+        return;
     }
+
 
     public List<List<Integer>> levelOrder(TreeNode root) {
 
-        if(root == null){
-            return new ArrayList<>();
-        }
+        // Time Complexity : O(n), where n is the number of nodes.
+        // The function will traverse the nodes exactly once, which makes the TC 
+        // equal to O(n).
+
+        // Space Complexity : O(height + n)
+        // At any time, the worst SC will be equal to the maximum height of the tree
+        // stored in the recursion stack as in DFS, the function will 
+        // travel through the height of the binary tree. The output array will have 
+        // every node in total, so it has a SC of O(n).
+
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        levelorderDFS(root, ans, 0);
+
+        return ans;
+
+
+
+
+        // // Time Complexity : O(n), where n is the number of nodes.
+        // // Each node will be traversed exactly once, so TC becomes O(n).
+
+        // // Space Complexity : O(2^(d + 1) + n), where d is the depth of the 
+        // // binary tree.
+        // // The maximum a queue will store will be at the lowest depth of the 
+        // // binary tree, if every node has two leaf nodes. The output array will 
+        // // have every node in total, so it has a SC of O(n).
+
         
-        List<List<Integer>> list = new ArrayList<>();
-
-        levelOrderTraversal(root, 0, list);
-
-        return list;
-
-
-
-
-
-
-
-        // List<List<Integer>> list = new ArrayList<>();
+        // List<List<Integer>> ans = new ArrayList<>();
         // Queue<TreeNode> queue = new LinkedList<>();
 
         // if(root == null){
-        //     return new ArrayList<>();
+        //     return ans;
         // }
+
         // queue.add(root);
 
         // while(!queue.isEmpty()){
+
         //     int size = queue.size();
-        //     List<Integer> temp = new ArrayList<>();
+        //     List<Integer> list = new ArrayList<>();
         //     while(size > 0){
         //         TreeNode curr = queue.poll();
-        //         temp.add(curr.val);
+        //         list.add(curr.val);
+
         //         if(curr.left != null){
         //             queue.add(curr.left);
         //         }
         //         if(curr.right != null){
         //             queue.add(curr.right);
         //         }
+
         //         size--;
         //     }
-        //     list.add(temp);
+
+        //     ans.add(list);
         // }
 
-        // return list;
+        // return ans;
     }
 }
