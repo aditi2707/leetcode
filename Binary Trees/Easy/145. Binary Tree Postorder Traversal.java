d72dbb14657ed033bead8df4076c5aabdef848ac
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-03-25 19:33:13 -0700

    Update 145. Binary Tree Postorder Traversal.java
    Link to the question:
    https://leetcode.com/problems/binary-tree-postorder-traversal/

--- Binary Trees/Easy/145. Binary Tree Postorder Traversal.java
+++ Binary Trees/Easy/145. Binary Tree Postorder Traversal.java
@@ -15,18 +15,38 @@
  */
 class Solution {
 
-    public void postOrder(TreeNode root, List<Integer> arr){
+    private void postorder(TreeNode root, List<Integer> ans){
 
-        if(root != null){
-            postOrder(root.left, arr);
-            postOrder(root.right, arr);
-            arr.add(root.val);
+        if(root == null){
+            return;
         }
+
+        postorder(root.left, ans);
+        postorder(root.right, ans);
+        ans.add(root.val);
+
+        return;
     }
+
+
     public List<Integer> postorderTraversal(TreeNode root) {
+
+        // Time Complexity : O(log n)(best case) and O(n)(worst case), where n is the
+        // number of nodes.
+        // The function will traverse every node exactly once. In the best case, the
+        // tree is perfectly balanced which gives a TC of O(height) of the tree, 
+        // hence O(log n). In the worst case when the tree is completely skewed, 
+        // then the height becomes the total number of nodes, hence TC is O(n).
+
+        // Space Complexity : O(n + n), where n is the number of nodes.
+        // If the tree is skewed, then the recursion stack can hold all the nodes at
+        // one moment. So, SC becomes O(n). An output array is used to store the 
+        // result which will store all the nodes.
+        
         
         List<Integer> ans = new ArrayList<>();
-        postOrder(root, ans);
+
+        postorder(root, ans);
 
         return ans;
     }
