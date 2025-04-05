7cdad36ff1d25a2eee84e491cbc3ab9e91cd0539
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-04-05 12:16:31 -0700

    Update 371. Sum of Two Integers.java
    Link to the question:
    https://leetcode.com/problems/sum-of-two-integers/

--- Bit Manipulation/Medium/371. Sum of Two Integers.java
+++ Bit Manipulation/Medium/371. Sum of Two Integers.java
@@ -1,10 +1,16 @@
 class Solution {
     public int getSum(int a, int b) {
+
+        // Time Complexity : O(1)
+
+        // Space Complexity : O(1)
+        
         
         while(b != 0){
-            int temp = (a & b) << 1;
-            a = a ^ b;
-            b = temp; 
+            int xor = a ^ b;
+            int and = (a & b) << 1;
+            a = xor;
+            b = and;
         }
 
         return a;
