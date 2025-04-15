25a28beb93e6ca28ea6fdf146118367b13f99ab9
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-04-14 20:32:42 -0700

    Update 125. Valid Palindrome.java
    Link to the question:
    https://leetcode.com/problems/valid-palindrome/

--- Strings/Easy/125. Valid Palindrome.java
+++ Strings/Easy/125. Valid Palindrome.java
@@ -13,28 +13,70 @@ class Solution {
 
     public boolean isPalindrome(String s) {
 
-        s = s.toLowerCase();
+        // Time Complexity : O(n / 2)
+        // The loop will terminate when the two pointers will cross one other.
+
+        // Space Complexity : O(1)
+
+        
         int i = 0, j = s.length() - 1;
 
         while(i < j){
-            if(!Character.isLetterOrDigit(s.charAt(i))){
+            char start = s.charAt(i);
+            char end = s.charAt(j);
+
+            if(!Character.isLetter(start) && !Character.isDigit(start)){
                 i++;
+                continue;
             }
-            else if(!Character.isLetterOrDigit(s.charAt(j))){
+            if(!Character.isLetter(end) && !Character.isDigit(end)){
                 j--;
+                continue;
             }
-            else{
-                if(s.charAt(i) != s.charAt(j)){
-                    return false;
-                }
-                else{
-                    i++;
-                    j--;
-                }
-            }  
+
+            if(Character.isLetter(start)){
+                start = Character.toLowerCase(start);
+            }
+            if(Character.isLetter(end)){
+                end = Character.toLowerCase(end);
+            }
+            
+            if(start != end){
+                return false;
+            }
+
+            i++;
+            j--;
         }
 
         return true;
+
+
+
+        
+
+        // s = s.toLowerCase();
+        // int i = 0, j = s.length() - 1;
+
+        // while(i < j){
+        //     if(!Character.isLetterOrDigit(s.charAt(i))){
+        //         i++;
+        //     }
+        //     else if(!Character.isLetterOrDigit(s.charAt(j))){
+        //         j--;
+        //     }
+        //     else{
+        //         if(s.charAt(i) != s.charAt(j)){
+        //             return false;
+        //         }
+        //         else{
+        //             i++;
+        //             j--;
+        //         }
+        //     }  
+        // }
+
+        // return true;
 
 
 
