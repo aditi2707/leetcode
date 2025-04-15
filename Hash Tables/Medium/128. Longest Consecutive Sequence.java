2c2b670addb9e6c7c057962b9a4505bf69f16bf2
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-04-14 19:13:16 -0700

    Update 128. Longest Consecutive Sequence.java
    Link to the question:
    https://leetcode.com/problems/longest-consecutive-sequence/

--- Hash Tables/Medium/128. Longest Consecutive Sequence.java
+++ Hash Tables/Medium/128. Longest Consecutive Sequence.java
@@ -1,43 +1,45 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
 
+        // Time Complexity : O(n)
+        // It seems like the TC should be quadratic but for every element, both the
+        // loops will run atmost twice, so TC ~ O(n).
+
+        // Space Complexity : O(n)
+        // The set will contain at max all elements of num (if they are unique)
+
+
         if(nums.length == 0){
             return 0;
         }
         
         Set<Integer> set = new HashSet<>();
-        int length = 1;
+        int max = 1;
 
         for(Integer i: nums){
             set.add(i);
         }
 
-        for(int i = 0; i < nums.length; i++){
-
-            if(!set.contains(nums[i]-1)){
-                int n = nums[i];
-                int temp = 0;
-
-                while(set.contains(n)){
-                    temp++;
-                    n++;
+        for(Integer i: set){
+            if(!set.contains(i - 1)){
+                int num = i;
+                int count = 0;
+                while(set.contains(num)){
+                    count++;
+                    num++;
                 }
-
-                length = Math.max(length, temp);
+                max = Math.max(max, count);
             }
         }
+
+        return max;
+
+
+
+        // // Time Complexity : O(nlog n + n)
+
+        // // Space Complexity : O(1)
         
-
-        return length;
-
-
-
-
-
-
-
-
-
         
         // if(nums.length == 0){
         //     return 0;
