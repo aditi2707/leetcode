9a20cf82997d04b40e1597b70247e6b0a5af987d
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-04-16 13:36:31 -0700

    Update 704. Binary Search.java
    Link to the question:
    https://leetcode.com/problems/binary-search/

--- Binary Search/Easy/704. Binary Search.java
+++ Binary Search/Easy/704. Binary Search.java
@@ -1,12 +1,16 @@
 class Solution {
     public int search(int[] nums, int target) {
+
+        // Time Complexity : O(log n)
+        // In every iteration, the length of the array is decreasing by half.
+
+        // Space Complexity : O(1)
+
         
-        int low = 0, high = nums.length-1;
+        int low = 0, high = nums.length - 1;
 
         while(low <= high){
-
-            int mid = (low + high) / 2;
-
+            int mid = low + (high - low) / 2;
             if(nums[mid] == target){
                 return mid;
             }
