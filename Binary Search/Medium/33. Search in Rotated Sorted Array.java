8a9abfb94be2be5c1b90241e100601b12244cc3d
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-04-18 11:51:30 -0700

    Update 33. Search in Rotated Sorted Array.java
    Link to the question:
    https://leetcode.com/problems/search-in-rotated-sorted-array/

--- Binary Search/Medium/33. Search in Rotated Sorted Array.java
+++ Binary Search/Medium/33. Search in Rotated Sorted Array.java
@@ -1,18 +1,20 @@
 class Solution {
     public int search(int[] nums, int target) {
 
-        // TC : O(logn)
-        // SC : O(1)
+        // Time Complexity : O(log n)
+        // This is a basic binary search code which takes O(log n) time.
 
+        // Space Complexity : O(1)
+        
+        
         int low = 0, high = nums.length - 1;
 
         while(low <= high){
-            int mid = low + (high - low)/2;
-
+            int mid = low + (high - low) / 2;
             if(nums[mid] == target){
                 return mid;
             }
-            if(nums[mid] >= nums[low]){
+            else if(nums[mid] >= nums[low]){
                 if(target >= nums[low] && target < nums[mid]){
                     high = mid - 1;
                 }
@@ -21,11 +23,11 @@ class Solution {
                 }
             }
             else{
-                if(target > nums[mid] && target <= nums[high]){
+                if(target <= nums[high] && target > nums[mid]){
                     low = mid + 1;
                 }
                 else{
-                    high = mid - 1;
+                    high= mid - 1;
                 }
             }
         }
