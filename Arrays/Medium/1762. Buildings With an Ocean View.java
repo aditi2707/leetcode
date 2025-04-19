1c233ba7b3bbb0aaf5bfb91518a6e54e15f3e5bb
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-04-19 14:35:06 -0700

    Update 1762. Buildings With an Ocean View.java
    Link to the question:
    https://leetcode.com/problems/buildings-with-an-ocean-view/

--- Arrays/Medium/1762. Buildings With an Ocean View.java
+++ Arrays/Medium/1762. Buildings With an Ocean View.java
@@ -1,23 +1,83 @@
 class Solution {
     public int[] findBuildings(int[] heights) {
 
-        int max = heights[heights.length-1];
-        List<Integer> ans = new ArrayList<>();
+        // Time Complexity : O((n * 2) + n)
 
-        for(int i = heights.length-2; i >= 0; i--){
-            if(heights[i] > max){
-                max = heights[i];
-                ans.add(i);
+        // Space Complexity : O(n) (ignoring the output array)
+
+
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0; i < heights.length; i++){
+            while(!stack.isEmpty() && heights[i] >= heights[stack.peek()]){
+                stack.pop();
             }
+            stack.push(i);
         }
 
-        int[] arr = new int[ans.size()+1];
+        int[] ans = new int[stack.size()];
 
-        for(int i = 0; i < arr.length-1; i++){
-            arr[i] = ans.get(ans.size() - 1- i);
+        for(int i = ans.length - 1; i >= 0; i--){
+            ans[i] = stack.pop();
         }
 
-        arr[arr.length-1] = heights.length-1;
-        return arr;
+        return ans;
+
+        
+
+        
+        // // Time Complexity : O(n + n)
+
+        // // Space Complexity : O(n) (ignoring the output array)
+        
+        
+        // int max = heights[heights.length-1];
+        // List<Integer> ans = new ArrayList<>();
+
+        // for(int i = heights.length-2; i >= 0; i--){
+        //     if(heights[i] > max){
+        //         max = heights[i];
+        //         ans.add(i);
+        //     }
+        // }
+
+        // int[] arr = new int[ans.size()+1];
+
+        // for(int i = 0; i < arr.length-1; i++){
+        //     arr[i] = ans.get(ans.size() - 1- i);
+        // }
+
+        // arr[arr.length-1] = heights.length-1;
+        // return arr;
+
+        
+
+
+        
+        // // Time Complexity : O(n + n)
+
+        // // Space Complexity : O(n + n) (ignoring the output array)
+
+        
+        // Stack<Integer> stack = new Stack<>();
+        // List<Integer> list = new ArrayList<>();
+
+        // stack.push(heights.length - 1);
+        // list.add(heights.length - 1);
+
+        // for(int i = heights.length - 2; i >= 0; i--){
+        //     if(heights[i] > heights[stack.peek()]){
+        //         list.add(i);
+        //         stack.push(i);
+        //     }
+        // }
+
+        // int[] ans = new int[list.size()];
+
+        // for(int i = list.size() - 1; i >= 0; i--){
+        //     ans[ans.length - 1 - i] = list.get(i);
+        // }
+
+        // return ans;
     }
 }
