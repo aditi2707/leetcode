86f85123fa8df5073bd8c92c8558dd6fe87e6d21
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-04-20 19:56:13 -0700

    Update 739. Daily Temperatures.java
    Link to the question:
    https://leetcode.com/problems/daily-temperatures/

--- Stack/Medium/739. Daily Temperatures.java
+++ Stack/Medium/739. Daily Temperatures.java
@@ -1,24 +1,54 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
+
+        // Time Complexity : O(n * 2)
+        // Focusing on only the while loop, every value will be pushed and popped 
+        // atmost once. So the TC is n * 2.
+
+        // Space Complexity : O(n) (ignoring the output array)
+        // The stack may have all the values if the array is in non-decreasing order
         
-        int[] ans = new int[temperatures.length];
+
         Stack<Integer> stack = new Stack<>();
+        int[] ans = new int[temperatures.length];
+        
+        stack.push(temperatures.length - 1);
 
-        stack.push(0);
-
-        for(int i = 1; i < temperatures.length; i++){
-            if(temperatures[i] > temperatures[stack.peek()]){
-                while(!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]){
-                    int j = stack.pop();
-                    ans[j] = i - j;
-                } 
-                stack.push(i);
+        for(int i = temperatures.length - 2; i >= 0; i--){
+            while(!stack.isEmpty() && temperatures[i] >= temperatures[stack.peek()]){
+                stack.pop();
             }
-            else{
-                stack.push(i);
+            if(!stack.isEmpty()){
+                ans[i] = stack.peek() - i;
             }
+            stack.push(i);
         }
 
         return ans;
+
+
+
+
+        // // Time Complexity : O(n * 2)
+        // // Focusing on only the while loop, every value will be pushed and popped 
+        // // atmost once. So the TC is n * 2.
+
+        // // Space Complexity : O(n) (ignoring the output array)
+        // // The stack may have all the values if the array is in non-decreasing order
+
+
+        // Stack<Integer> stack = new Stack<>();
+        // int[] ans = new int[temperatures.length];
+
+        // for(int i = 0; i < temperatures.length; i++){
+        //     while(!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]){
+        //         ans[stack.peek()] = i - stack.peek();
+        //         stack.pop();
+        //     }
+                
+        //     stack.push(i);
+        // }
+
+        // return ans;
     }
 }
