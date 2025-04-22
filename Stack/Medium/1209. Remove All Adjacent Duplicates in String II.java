038b2190870962a6b59f0674e94bf642f66d088e
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-04-22 13:23:38 -0700

    Update 1209. Remove All Adjacent Duplicates in String II.java
    Link to the question:
    https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string-ii/

--- Stack/Medium/1209. Remove All Adjacent Duplicates in String II.java
+++ Stack/Medium/1209. Remove All Adjacent Duplicates in String II.java
@@ -2,43 +2,37 @@ class Solution {
     public String removeDuplicates(String s, int k) {
 
         // Time Complexity : O(n + n)
+        
 
         // Space Complexity : O(n)
-        
+
         
         Stack<Pair<Character, Integer>> stack = new Stack<>();
-        
-        int index = 0;
-        String ans = "";
+        StringBuilder ans = new StringBuilder();
 
-        while(index < s.length()){
-            char ch = s.charAt(index);
-
-            if(stack.isEmpty() || stack.peek().getKey() != ch){
-                stack.push(new Pair<>(ch, 1));
+        for(char c: s.toCharArray()){
+            if(stack.isEmpty() || stack.peek().getKey() != c){
+                stack.push(new Pair<>(c, 1));
             }
             else{
-                int count = stack.pop().getValue();
-                stack.push(new Pair<>(ch, count + 1));
-                if(stack.peek().getValue() == k){
-                    // while(!stack.isEmpty() && stack.peek().getKey() == ch){
-                    //     stack.pop();
-                    // }
-                    stack.pop();
-                }
+                Pair<Character, Integer> pair = stack.pop();
+                stack.push(new Pair<>(c, pair.getValue() + 1));
             }
 
-            index++;
+            if(stack.peek().getValue() == k){
+                stack.pop();
+            }
         }
 
         while(!stack.isEmpty()){
-            Pair<Character, Integer> pop = stack.pop();
-            for(int i = 0; i < pop.getValue(); i++){
-                ans = pop.getKey() + ans;
+            Pair<Character, Integer> pair = stack.pop();
+            StringBuilder temp = new StringBuilder();
+            for(int i = 0; i < pair.getValue(); i++){
+                temp.append(pair.getKey());
             }
-            
+            ans.append(temp);
         }
 
-        return ans;
+        return ans.reverse().toString();
     }
 }
