8f728970acdfbf6454b28328752d4307b0a69565
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-04-25 13:12:43 -0700

    Update 73. Set Matrix Zeroes.java
    Link to the question:
    https://leetcode.com/problems/set-matrix-zeroes/

--- Matrix/Medium/73. Set Matrix Zeroes.java
+++ Matrix/Medium/73. Set Matrix Zeroes.java
@@ -1,40 +1,54 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
 
-        int start = 1;
+        // Time Complexity : O((m * 2) + (n * 2) + (m * n) ^ 2) 
+        // (where m = rows, n = cols).
+
+        // Space Complexity : O(1)
+        
+        
+        boolean firstRow = false;
+        boolean firstCol = false;
 
         for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix[i].length; j++){
+            if(matrix[i][0] == 0){
+                firstCol = true;
+                break;
+            }
+        }
+        for(int i = 0; i < matrix[0].length; i++){
+            if(matrix[0][i] == 0){
+                firstRow = true;
+                break;
+            }
+        }
+
+        for(int i = 1; i < matrix.length; i++){
+            for(int j = 1; j < matrix[0].length; j++){
                 if(matrix[i][j] == 0){
-                    if(i == 0){
-                        start = 0;
-                    }
-                    else{
-                        matrix[i][0] = 0;
-                    }
+                    matrix[i][0] = 0;
                     matrix[0][j] = 0;
                 }
             }
         }
 
         for(int i = 1; i < matrix.length; i++){
-            if(matrix[i][0] == 0){
-                for(int j = 1; j < matrix[0].length; j++){
+            for(int j = 1; j < matrix[0].length; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
                     matrix[i][j] = 0;
                 }
             }
         }
 
-        for(int j = 0; j < matrix[0].length; j++){
-            if(matrix[0][j] == 0){
-                for(int i = 1; i < matrix.length; i++){
-                    matrix[i][j] = 0;
-                }
+        if(firstRow){
+            for(int i = 0; i < matrix[0].length; i++){
+                matrix[0][i] = 0;
             }
         }
-
-        if(start == 0){
-            Arrays.fill(matrix[0], 0);
+        if(firstCol){
+            for(int i = 0; i < matrix.length; i++){
+                matrix[i][0] = 0;
+            }
         }
 
         return;
@@ -42,10 +56,11 @@ class Solution {
 
 
 
+        // // Time Complexity : O((m * n) ^ 3) (where m = rows, n = cols)
 
+        // // Space Complexity : O(m + n) (where m = rows, n = cols)
 
-
-
+        
         // Set<Integer> rows = new HashSet<>();
         // Set<Integer> cols = new HashSet<>();
 
@@ -58,14 +73,14 @@ class Solution {
         //     }
         // }
 
-        // for(int i = 0; i < matrix.length; i++){
-        //     for(int j = 0; j < matrix[i].length; j++){
-        //         if(rows.contains(i)){
-        //             matrix[i][j] = 0;
-        //         }
-        //         if(cols.contains(j)){
-        //             matrix[i][j] = 0;
-        //         }
+        // for(Integer i: rows){
+        //     for(int j = 0; j < matrix[0].length; j++){
+        //         matrix[i][j] = 0;
+        //     }
+        // }
+        // for(Integer j: cols){
+        //     for(int i = 0; i < matrix.length; i++){
+        //         matrix[i][j] = 0;
         //     }
         // }
 
