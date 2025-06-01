ae20dd1771bb3c03cb6f0e4c2c9695f9110c2614
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-05-31 19:55:23 -0700

    Update 1732. Find the Highest Altitude.java
    Link to the question:
    https://leetcode.com/problems/find-the-highest-altitude/

--- Prefix Sum/Easy/1732. Find the Highest Altitude.java
+++ Prefix Sum/Easy/1732. Find the Highest Altitude.java
@@ -1,16 +1,38 @@
 class Solution {
     public int largestAltitude(int[] gain) {
+
+        // Time Complexity : O(n)
+
+        // Space Complexity : O(1)
         
-        int maxGain = 0;
-        int altitude = 0;
+        
+        int prefix = 0;
+        int max = 0;
 
         for(int i = 0; i < gain.length; i++){
-            altitude += gain[i];
-            if(altitude > maxGain){
-                maxGain = altitude;
-            }
+            prefix += gain[i];
+            max = Math.max(max, prefix);
         }
 
-        return maxGain;
+        return max;
+
+
+
+
+
+        // // Time Complexity : O(n)
+
+        // // Space Complexity : O(n)
+
+        
+        // int[] prefix = new int[gain.length + 1];
+        // int max = 0;
+
+        // for(int i = 0; i < gain.length; i++){
+        //     prefix[i + 1] = prefix[i] + gain[i];
+        //     max = Math.max(max, prefix[i + 1]);
+        // }
+
+        // return max;
     }
 }
