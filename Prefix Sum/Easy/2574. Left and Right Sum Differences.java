144a5d2b84465aee5966d48bb37961535e99b9d8
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-05-31 19:37:59 -0700

    Update 2574. Left and Right Sum Differences.java
    Link to the question:
    https://leetcode.com/problems/left-and-right-sum-differences/

--- Prefix Sum/Easy/2574. Left and Right Sum Differences.java
+++ Prefix Sum/Easy/2574. Left and Right Sum Differences.java
@@ -1,27 +1,50 @@
 class Solution {
     public int[] leftRightDifference(int[] nums) {
+
+        // Time Complexity : O(n * 2)
+
+        // Space Complexity : O(1) (if we ignore the output array)
         
-        int totalLeft = 0;
-        int totalRight = 0;
+
+        int left = 0, right = 0;
         int[] ans = new int[nums.length];
 
-        for(int i = 1; i < nums.length; i++){
-            totalRight += nums[i];
+        for(Integer i: nums){
+            left += i;
         }
 
-        ans[0] = totalRight;
+        ans[nums.length - 1] = left - nums[nums.length - 1];
+        left -= nums[nums.length - 1];
 
-        for(int i = 1; i < nums.length; i++){
-
-            if(i == nums.length-1){
-                ans[i] = totalLeft + nums[i-1];
-                continue;
-            }
-            totalLeft += nums[i-1];
-            totalRight -= nums[i];
-            ans[i] = Math.abs(totalLeft - totalRight);
+        for(int i = nums.length - 2; i >= 0; i--){
+            right += nums[i + 1];
+            left -= nums[i];
+            ans[i] = Math.abs(left - right);
         }
 
         return ans;
+
+
+
+
+
+        
+        // // Time Complexity : O(n * 2)
+
+        // // Space Complexity : O(n * 2)
+
+
+        // int[] left = new int[nums.length];
+        // int[] right = new int[nums.length];
+
+        // for(int i = 1; i < nums.length; i++){
+        //     left[i] = left[i - 1] + nums[i - 1];
+        // }
+        // for(int i = nums.length - 2; i >= 0; i--){
+        //     right[i] = right[i + 1] + nums[i + 1];
+        //     left[i] = Math.abs(left[i] - right[i]);
+        // }
+
+        // return left;
     }
 }
