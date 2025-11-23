98de3ee3fb8bbb59c9031f9fc27279c91ff4fa0e
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-11-22 22:17:16 -0800

    Update 20. Valid Parentheses.java
    Link to the question:
    https://leetcode.com/problems/valid-parentheses/description/

--- Stack/Easy/20. Valid Parentheses.java
+++ Stack/Easy/20. Valid Parentheses.java
@@ -1,6 +1,18 @@
 class Solution {
     public boolean isValid(String s) {
 
+        /* 
+        Edge cases:
+        1. Starting with a closed bracket - returns false
+        2. Only opening brackets - returns false
+        3. Opening brackets != Closing brackets - returns false
+
+        If a stack is used, then the order of opening and closing will be checked.
+        And all the above conditions.
+        */
+
+        
+
         // Time Complexity : O(n)
         // The while loop will run for the entirety of the string if the string is
         // valid.
@@ -29,6 +41,8 @@ class Solution {
                 else if(stack.peek() == '[' && ch == ']'){
                     stack.pop();
                 }
+                // This is for type mismatch (closing with some other bracket)
+                // Or all closing brackets
                 else{
                     return false;
                 }
