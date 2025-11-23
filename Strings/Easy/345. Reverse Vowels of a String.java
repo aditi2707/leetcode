4645c26cf77bef401e4c69503ee95230c0180418
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-11-22 23:17:26 -0800

    Update 345. Reverse Vowels of a String.java
    Link to the question:
    https://leetcode.com/problems/reverse-vowels-of-a-string/description/

--- Strings/Easy/345. Reverse Vowels of a String.java
+++ Strings/Easy/345. Reverse Vowels of a String.java
@@ -1,6 +1,44 @@
 class Solution {
     public String reverseVowels(String s) {
 
+
+        // Time Complexity : O(n + 10)
+        // If all the letters are composite, the loop will run for all n letters
+
+        // Space Complexity : O(n) (if we consider the char array)
+        
+        
+        int i = 0, j = s.length() - 1;
+        String vowels = "AEIOUaeiou";
+        char[] arr = s.toCharArray();
+
+        while(i < j){
+            char ch1 = arr[i];
+            char ch2= arr[j];
+
+            if(vowels.indexOf(ch1) != -1 && vowels.indexOf(ch2) != -1){
+                char temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+
+                i++;
+                j--;
+            }
+            else if(vowels.indexOf(ch1) == -1){
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+
+        return new String(arr);
+
+
+
+
+        
+
         // Time Complexity : O(n + 10 + n + n)
         // The string functions may take upto linear TC.
 
