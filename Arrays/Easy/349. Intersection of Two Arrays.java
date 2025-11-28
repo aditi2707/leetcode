dbf1e5ad7954b3ce6dbadb19b8fcccef29b728a7
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-11-27 20:38:44 -0800

    Update 349. Intersection of Two Arrays.java
    Link to the question:
    https://leetcode.com/problems/intersection-of-two-arrays/

--- Arrays/Easy/349. Intersection of Two Arrays.java
+++ Arrays/Easy/349. Intersection of Two Arrays.java
@@ -1,5 +1,43 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
+
+        // Time Complexity : O(n + 1000)
+
+        // Space Complexity : O(1001)
+        
+        
+        int[] freq = new int[1001];
+        int count = 0;
+
+        for(int i = 0; i < nums1.length; i++){
+            if(freq[nums1[i]] == 0){
+                freq[nums1[i]] = 1;
+            }
+        }
+
+        for(int i = 0; i < nums2.length; i++){
+            if(freq[nums2[i]] == 1){
+                freq[nums2[i]] = 2;
+                count++;
+            }
+        }
+
+        int[] ans = new int[count];
+        int index = 0;
+
+        for(int i = 0; i < freq.length; i++){
+            if(freq[i] == 2){
+                ans[index] = i;
+                index++;
+            }
+        }
+
+        return ans;
+
+
+
+
+        
         
         HashMap<Integer, Integer> num1 = new HashMap<>();
         List<Integer> ans = new ArrayList<>();
