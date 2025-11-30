e67593ee1b5a2d61b3b59988f7f98b6822b3e668
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-11-30 10:34:11 -0800

    Update 383. Ransom Note.java
    Link to the question:
    https://leetcode.com/problems/ransom-note/

--- Hash Tables/Easy/383. Ransom Note.java
+++ Hash Tables/Easy/383. Ransom Note.java
@@ -1,6 +1,11 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
 
+        // Time Complexity : O(n + m + 26)
+
+        // Space Complexity : O(26)
+
+
         int[] freq = new int[26];
 
         for(int i = 0; i < magazine.length(); i++){
