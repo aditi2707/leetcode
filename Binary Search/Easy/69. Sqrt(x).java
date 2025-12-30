74b62c2c87cfb06741b03d36b564595be118368a
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-12-30 11:09:20 -0800

    Update 69. Sqrt(x).java
    Link to the question:
    https://leetcode.com/problems/sqrtx/

--- Binary Search/Easy/69. Sqrt(x).java
+++ Binary Search/Easy/69. Sqrt(x).java
@@ -1,24 +1,70 @@
 class Solution {
     public int mySqrt(int x) {
 
-        long low = 1, high = x;
+        // Edge cases:
+        // 1. The while loop will not run for x = 0 and x = 1. So that have to
+        // be handled by the base cases.
+        
+
+        // Time Complexity : O(log (x / 2))
+
+        // Space Complexity : O(1)
+        
+
+        if(x == 0 || x == 1){
+            return x;
+        }
+        
+        long low = 1, high = x / 2;
 
         while(low <= high){
+            long mid = (low + high) / 2;
 
-            long mid = low + (high - low)/2;
-
-            if(mid * mid == x){
+            if((mid * mid) == (long)x){
                 return (int)mid;
             }
-            else if(mid * mid > x){
-                high = mid - 1;
+            else if((mid * mid) < (long)x){
+                low = mid + 1;
             }
             else{
-                low = mid + 1;
+                high = mid - 1;
             }
         }
 
-        return (int)low-1;
+        return (int)high;
+
+
+
+
+
+
+        // // Time Complexity : O(log x)
+
+        // // Space Complexity : O(1)
+
+
+        // long low = 1, high = x;
+
+        // while(low <= high){
+        //     long mid = (low + high) / 2;
+
+        //     if((mid * mid) == (long)x){
+        //         return (int)mid;
+        //     }
+        //     else if((mid * mid) < (long)x){
+        //         low = mid + 1;
+        //     }
+        //     else{
+        //         high = mid - 1;
+        //     }
+        // }
+
+        // return (int)high;
+
+
+
+
+        
 
         // return (int)(Math.sqrt(x));
     }
