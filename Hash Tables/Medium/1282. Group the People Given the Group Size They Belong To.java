124eef96114feab9c0cbf894414919949f3cb1cd
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-12-30 20:18:43 -0800

    Update 1282. Group the People Given the Group Size They Belong To.java
    Link to the question:
    https://leetcode.com/problems/group-the-people-given-the-group-size-they-belong-to/

--- Hash Tables/Medium/1282. Group the People Given the Group Size They Belong To.java
+++ Hash Tables/Medium/1282. Group the People Given the Group Size They Belong To.java
@@ -1,5 +1,46 @@
 class Solution {
     public List<List<Integer>> groupThePeople(int[] groupSizes) {
+
+        // Time Complexity : O(n * 2)
+
+        // Space Complexity : O(n)
+        
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        Map<Integer, List<Integer>> map = new HashMap<>();
+
+        for(int i = 0; i < groupSizes.length; i++){
+            if(!map.containsKey(groupSizes[i])){
+                map.put(groupSizes[i], new ArrayList<>());
+            }
+            map.get(groupSizes[i]).add(i);
+        }
+
+        int index = 0;
+
+        for(Integer g: map.keySet()){
+            List<Integer> l = map.get(g);
+            int count = 0;
+            for(int i = 0; i < l.size(); i++){
+                if(count == 0){
+                    ans.add(new ArrayList<>());
+                }
+                ans.get(index).add(l.get(i));
+                count++;
+
+                if(count == g){
+                    index++;
+                    count = 0;
+                }
+            }
+        }
+
+        return ans;
+
+
+
+
+        
         
         HashMap<Integer, List<Integer>> map = new HashMap<>();
         List<List<Integer>> list = new ArrayList<>();
