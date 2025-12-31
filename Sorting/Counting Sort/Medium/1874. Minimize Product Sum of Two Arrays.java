8d5c467adbdcf3a03510e8f1c34509767cf0060c
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2025-12-30 19:13:24 -0800

    Create 1874. Minimize Product Sum of Two Arrays.java
    Link to the question:
    https://leetcode.com/problems/minimize-product-sum-of-two-arrays/

---
+++ Sorting/Counting Sort/Medium/1874. Minimize Product Sum of Two Arrays.java
@@ -0,0 +1,64 @@
+class Solution {
+    public int minProductSum(int[] nums1, int[] nums2) {
+
+        // Time Complexity : O((n * 2) + (n * 2) + 200)
+
+        // Space Complexity : O(200)
+        
+        
+        int[] count1 = new int[101];
+        int[] count2 = new int[101];
+
+        int sum = 0;
+
+        for(int i = 0; i < nums2.length; i++){
+            count1[nums1[i]]++;
+            count2[nums2[i]]++;
+        }
+
+        int i = 0, j = 100;
+
+        while(i <= 100 && j >= 0){
+            if(count1[i] == 0){
+                i++;
+                continue;
+            }
+            if(count2[j] == 0){
+                j--;
+                continue;
+            }
+            
+            while(count1[i] > 0 && count2[j] > 0){
+                sum += i * j;
+                count1[i]--;
+                count2[j]--;
+            }
+        }
+        
+
+        return sum;
+
+
+
+
+
+
+
+
+        // // Time Complexity : O((nlog n) * 2 + n)
+
+        // // Space Complexity : O(1)
+
+        
+        // Arrays.sort(nums1);
+        // Arrays.sort(nums2);
+
+        // int sum = 0;
+
+        // for(int i = 0; i < nums1.length; i++){
+        //     sum += nums1[i] * nums2[nums2.length - 1 - i];
+        // }
+
+        // return sum;
+    }
+}
