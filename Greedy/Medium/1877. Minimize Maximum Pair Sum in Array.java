b1ef720b558a2dad5a0aa2484fbc7d15b936433e
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2026-01-01 15:50:51 -0800

    Update 1877. Minimize Maximum Pair Sum in Array.java
    Link to the question:
    https://leetcode.com/problems/minimize-maximum-pair-sum-in-array/

--- Greedy/Medium/1877. Minimize Maximum Pair Sum in Array.java
+++ Greedy/Medium/1877. Minimize Maximum Pair Sum in Array.java
@@ -1,5 +1,57 @@
 class Solution {
     public int minPairSum(int[] nums) {
+
+        // Time Complexity : O((n * 2) + (max + n))
+
+        // Space Complexity : O(max)
+
+
+        int max = 0, maxSum = 0;
+
+        for(Integer i: nums){
+            max = Math.max(max, i);
+        }
+
+        int[] arr = new int[max + 1];
+
+        for(Integer i: nums){
+            arr[i] += 1;
+        }
+        
+        int a = 0, b = max;
+
+        while(a <= b){
+            if(arr[a] == 0){
+                a++;
+                continue;
+            }
+            if(arr[b] == 0){
+                b--;
+                continue;
+            }
+            maxSum = Math.max(maxSum, a + b);
+            arr[a] -= 1;
+            arr[b] -= 1;
+            if(arr[a] == 0){
+                a++;
+            }
+            if(arr[b] == 0){
+                b--;
+            }
+        }
+
+        return maxSum;
+
+
+
+
+
+
+
+        // // Time Complexity : O(nlog n + n)
+
+        // // Space Complexity : O(1)
+        
         
         Arrays.sort(nums);
         int maxSum = 0;
