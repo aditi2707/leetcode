51febb34128604aec651b8d00ae6f58fc1a1ae7f
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2026-01-02 19:38:19 -0800

    Update 3205. Maximum Array Hopping Score I.java
    Link to the question:
    https://leetcode.com/problems/maximum-array-hopping-score-i/

--- Dynamic Programming/Medium/3205. Maximum Array Hopping Score I.java
+++ Dynamic Programming/Medium/3205. Maximum Array Hopping Score I.java
@@ -1,23 +1,66 @@
 class Solution {
-    public int maxScore(int[] nums) {
 
-        // int max = 0, ans = 0;
+    private int hoppingScore(int[] nums, int index, int[] dp){
 
-        // for(int i = nums.length-1; i > 0; i--){
-        //     max = Math.max(max, nums[i]);
-        //     ans += max;
-        // }
-
-        // return ans;
-        
-        int[] dp = new int[nums.length];
-
-        for(int i = 1; i < nums.length; i++){
-            for(int j = 0; j < i; j++){
-                dp[i] = Math.max(dp[i], dp[j]+(i-j)*nums[i]);
-            }
+        if(index <= 0){
+            return 0;
         }
 
-        return dp[nums.length-1];
+        if(dp[index] != 0){
+            return dp[index];
+        }
+
+        int res = 0;
+
+        for(int i = 0; i < index; i++){
+            res = Math.max(res, (index - i) * nums[index] + hoppingScore(nums, i, dp));
+        }
+
+        return dp[index] = res;
+    }
+
+    
+    public int maxScore(int[] nums) {
+
+        // Time Complexity : O(n)
+
+        // Space Complexity : O(1)
+        
+
+        int max = 0, ans = 0;
+
+        for(int i = nums.length-1; i > 0; i--){
+            max = Math.max(max, nums[i]);
+            ans += max;
+        }
+
+        return ans;
+
+
+
+
+        
+        // int[] dp = new int[nums.length];
+
+        // for(int i = 1; i < nums.length; i++){
+        //     for(int j = 0; j < i; j++){
+        //         dp[i] = Math.max(dp[i], dp[j]+(i-j)*nums[i]);
+        //     }
+        // }
+
+        // return dp[nums.length-1];
+
+
+
+        
+
+        // // Time Complexity : O(n * n)
+
+        // // Space Complexity : O(n + n)
+
+
+        // int[] dp = new int[nums.length];
+        
+        // return hoppingScore(nums, nums.length - 1, dp);
     }
 }
