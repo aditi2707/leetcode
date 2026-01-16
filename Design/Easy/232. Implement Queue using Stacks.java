847249c3f184f6337083afc1ab653099a7ab7dc3
Aditi Srivastava <65551365+aditi2707@users.noreply.github.com>
2026-01-16 12:53:58 -0800

    Update 232. Implement Queue using Stacks.java
    Link to the question:
    https://leetcode.com/problems/implement-queue-using-stacks/

--- Design/Easy/232. Implement Queue using Stacks.java
+++ Design/Easy/232. Implement Queue using Stacks.java
@@ -2,54 +2,47 @@ class MyQueue {
 
     Stack<Integer> stack1;
     Stack<Integer> stack2;
-    int peek;
+    int front = -1;
 
     public MyQueue() {
         stack1 = new Stack<>();
-        stack2 = new Stack();
-        peek = 0;
+        stack2 = new Stack<>();
     }
     
     public void push(int x) {
 
-        if(empty()){
-            peek = x;
+        if(stack1.isEmpty()){
+            front = x;
         }
+        
         stack1.push(x);
+
         return;
     }
     
     public int pop() {
         
-        while(!stack1.isEmpty()){
-            stack2.push(stack1.pop());
+        if(stack2.isEmpty()){
+            while(!stack1.isEmpty()){
+                stack2.push(stack1.pop());
+            }
         }
 
-        int pop = stack2.pop();
-
-        if(!stack2.isEmpty()){
-            peek = stack2.peek();
-        }
-        
-        while(!stack2.isEmpty()){
-            stack1.push(stack2.pop());
-        }
-
-        return pop;
+        return stack2.pop(); 
     }
     
     public int peek() {
         
-        return peek;
+        if(stack2.isEmpty()){
+            return front;
+        }
+
+        return stack2.peek();
     }
     
     public boolean empty() {
         
-        if(stack1.isEmpty()){
-            return true;
-        }
-
-        return false;
+        return stack1.isEmpty() && stack2.isEmpty();
     }
 }
 
@@ -61,3 +54,76 @@ class MyQueue {
  * int param_3 = obj.peek();
  * boolean param_4 = obj.empty();
  */
+
+
+
+
+
+
+
+
+
+
+// class MyQueue {
+
+//     Stack<Integer> stack1;
+//     Stack<Integer> stack2;
+//     int peek;
+
+//     public MyQueue() {
+//         stack1 = new Stack<>();
+//         stack2 = new Stack();
+//         peek = 0;
+//     }
+    
+//     public void push(int x) {
+
+//         if(empty()){
+//             peek = x;
+//         }
+//         stack1.push(x);
+//         return;
+//     }
+    
+//     public int pop() {
+        
+//         while(!stack1.isEmpty()){
+//             stack2.push(stack1.pop());
+//         }
+
+//         int pop = stack2.pop();
+
+//         if(!stack2.isEmpty()){
+//             peek = stack2.peek();
+//         }
+        
+//         while(!stack2.isEmpty()){
+//             stack1.push(stack2.pop());
+//         }
+
+//         return pop;
+//     }
+    
+//     public int peek() {
+        
+//         return peek;
+//     }
+    
+//     public boolean empty() {
+        
+//         if(stack1.isEmpty()){
+//             return true;
+//         }
+
+//         return false;
+//     }
+// }
+
+// /**
+//  * Your MyQueue object will be instantiated and called as such:
+//  * MyQueue obj = new MyQueue();
+//  * obj.push(x);
+//  * int param_2 = obj.pop();
+//  * int param_3 = obj.peek();
+//  * boolean param_4 = obj.empty();
+//  */
